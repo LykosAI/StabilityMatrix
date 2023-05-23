d1049f4730f5a0a14eb040a0c0353133a7113083
Ionite <dev@ionite.io>
2023-05-23 19:15:25 -0400

    Update mainWindow name

--- StabilityMatrix/App.xaml.cs
+++ StabilityMatrix/App.xaml.cs
@@ -44,10 +44,10 @@ namespace StabilityMatrix
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
-            m_window.Activate();
+            mainWindow = new MainWindow();
+            mainWindow.Activate();
         }
 
-        private Window m_window;
+        private Window mainWindow;
     }
 }

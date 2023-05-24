eee5f09c86161555ba7509f82ea2e5651fc30c7a
Ionite <dev@ionite.io>
2023-05-23 21:21:21 -0400

    Add LaunchPage and NavigationView layout

---
+++ StabilityMatrix/LaunchPage.xaml.cs
@@ -0,0 +1,36 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace StabilityMatrix
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LaunchPage : Page
+    {
+        public LaunchPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void LaunchButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- StabilityMatrix/MainWindow.xaml.cs
+++ StabilityMatrix/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
 
 namespace StabilityMatrix
 {
@@ -26,9 +27,46 @@ namespace StabilityMatrix
         }
 
 
-        private void ButtonNavigate_OnClick(object sender, RoutedEventArgs e)
+        private void ButtonNavInstallPage_OnClick(object sender, RoutedEventArgs e)
         {
             ContentFrame.Navigate(typeof(InstallPage));
+        }
+
+        private void ButtonNavLaunchPage_OnClick(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(LaunchPage));
+        }
+
+        private void MainNavigationView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var home = MainNavigationView.MenuItems.OfType<NavigationViewItem>().First();
+            SetCurrentNavigationViewItem(home);
+        }
+
+        private void MainNavigationView_OnSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            SetCurrentNavigationViewItem(args.SelectedItemContainer as NavigationViewItem);
+        }
+
+        private void SetCurrentNavigationViewItem(NavigationViewItem item)
+        {
+            if (item == null || item.Tag == null) return;
+
+            var tag = item.Tag.ToString();
+            switch (tag)
+            {
+                case "InstallPage":
+                    ContentFrame.Navigate(typeof(InstallPage));
+                    break;
+                case "LaunchPage":
+                    ContentFrame.Navigate(typeof(LaunchPage));
+                    break;
+                default:
+                    throw new ArgumentException($"Invalid tag: {tag}");
+            }
+
+            MainNavigationView.Header = item.Content;
+            MainNavigationView.SelectedItem = item;
         }
     }
 }

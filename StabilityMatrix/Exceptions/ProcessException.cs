a5db5f4dd8153d6ddfcbcd7d9a3c86d3f8e93e89
Ionite <dev@ionite.io>
2023-05-25 14:46:46 -0400

    Add ProcessException

---
+++ StabilityMatrix/Exceptions/ProcessException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StabilityMatrix.Exceptions;
+
+/// <summary>
+/// Exception that is thrown when a process fails.
+/// </summary>
+public class ProcessException: Exception
+{
+    public ProcessException(string message) : base(message)
+    {
+    }
+}

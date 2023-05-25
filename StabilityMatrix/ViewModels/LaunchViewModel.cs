be01f20a81108500f550163ff7d90c85c2b0594e
JT <mohnjiles@gmail.com>
2023-05-25 14:31:40 -0700

    nevermind

--- StabilityMatrix/ViewModels/LaunchViewModel.cs
+++ StabilityMatrix/ViewModels/LaunchViewModel.cs
@@ -45,18 +45,6 @@ public partial class LaunchViewModel : ObservableObject
     public LaunchViewModel(ISettingsManager settingsManager)
     {
         this.settingsManager = settingsManager;
-        Packages.CollectionChanged += PackagesOnCollectionChanged;
-    }
-
-    private void PackagesOnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-    {
-        if (e.Action != NotifyCollectionChangedAction.Add) return;
-        
-        var newPackage = e.NewItems?.Cast<InstalledPackage>().FirstOrDefault();
-        if (newPackage != null)
-        {
-            settingsManager.AddInstalledPackage(newPackage);
-        }
     }
 
     public RelayCommand LaunchCommand => new(() =>

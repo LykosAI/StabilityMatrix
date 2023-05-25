8178be4d4196d7ca1e73755064036dce2ea8ac42
Ionite <dev@ionite.io>
2023-05-25 14:04:52 -0400

    Add Logger and traces to ProcessRunner

--- StabilityMatrix/Helper/ProcessRunner.cs
+++ StabilityMatrix/Helper/ProcessRunner.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using NLog;
 
 namespace StabilityMatrix.Helper;
 
 public static class ProcessRunner
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    
     public static async Task<string> GetProcessOutputAsync(string fileName, string arguments)
     {
+        Logger.Trace($"Starting process '{fileName}' with arguments '{arguments}'");
         using var process = new Process();
         process.StartInfo.FileName = fileName;
         process.StartInfo.Arguments = arguments;
@@ -24,6 +28,7 @@ public static class ProcessRunner
 
     public static Process StartProcess(string fileName, string arguments, Action<string?>? outputDataReceived = null)
     {
+        Logger.Trace($"Starting process '{fileName}' with arguments '{arguments}'");
         var process = new Process();
         process.StartInfo.FileName = fileName;
         process.StartInfo.Arguments = arguments;

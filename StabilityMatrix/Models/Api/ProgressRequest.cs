73613bb36b7f18f9dd322aafc12dedadb3e5d014
Ionite <dev@ionite.io>
2023-05-27 03:22:29 -0400

    Add Progress api models

---
+++ StabilityMatrix/Models/Api/ProgressRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StabilityMatrix.Models.Api;
+
+public class ProgressRequest
+{
+    [JsonPropertyName("skip_current_image")]
+    public bool? SkipCurrentImage { get; set; }
+}

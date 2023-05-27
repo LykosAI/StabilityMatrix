118bbc786e06a7a779e4f5b96f002fc94ecebf52
Ionite <dev@ionite.io>
2023-05-27 02:43:28 -0400

    Change prompt boxes to AutoSuggestBox

--- StabilityMatrix/TextToImagePage.xaml.cs
+++ StabilityMatrix/TextToImagePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Controls;
 using StabilityMatrix.ViewModels;
+using Wpf.Ui.Controls.AutoSuggestBoxControl;
 
 namespace StabilityMatrix;
 
@@ -9,5 +10,35 @@ public sealed partial class TextToImagePage : Page
     {
         InitializeComponent();
         DataContext = viewModel;
+    }
+
+    private void PositivePromptBox_OnQuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void PositivePromptBox_OnSuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void PositivePromptBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void NegativePromptBox_OnQuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void NegativePromptBox_OnSuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void NegativePromptBox_OnTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+    {
+        throw new System.NotImplementedException();
     }
 }

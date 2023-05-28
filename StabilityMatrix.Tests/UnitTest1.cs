9c943b7305bc4a790ba82695d6fefb4cd98ab6d2
Ionite <dev@ionite.io>
2023-05-28 18:10:22 -0400

    Add unit test project

---
+++ StabilityMatrix.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using StabilityMatrix;
+
+namespace StabilityMatrix.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(true);
+    }
+}

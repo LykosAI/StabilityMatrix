c00018dafcbb147973c9013ee19c4d73c0ca034f
Ionite <dev@ionite.io>
2023-05-29 14:59:07 -0400

    Add VladAutomatic package

---
+++ StabilityMatrix/Models/Packages/VladAutomatic.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using StabilityMatrix.Api;
+using StabilityMatrix.Helper;
+
+namespace StabilityMatrix.Models.Packages;
+
+public class VladAutomatic : BaseGitPackage
+{
+    public override string Name => "automatic";
+    public override string DisplayName { get; set; } = "SD.Next Web UI";
+    public override string Author => "vladmandic";
+    public override string LaunchCommand => "launch.py";
+    public override string DefaultLaunchArguments => $"{GetVramOption()} {GetXformersOption()}";
+
+    public VladAutomatic(IGithubApi githubApi, ISettingsManager settingsManager) : base(githubApi, settingsManager)
+    {
+    }
+
+    public override List<LaunchOptionDefinition> LaunchOptions => new()
+    {
+        new()
+        {
+            Name = "API",
+            Options = new() { "--api" }
+        },
+        new()
+        {
+            Name = "VRAM",
+            Options = new() { "--lowvram", "--medvram" }
+        },
+        new()
+        {
+            Name = "Xformers",
+            Options = new() { "--xformers" }
+        }
+    };
+
+    private static string GetVramOption()
+    {
+        var vramGb = HardwareHelper.GetGpuMemoryBytes() / 1024 / 1024 / 1024;
+
+        return vramGb switch
+        {
+            < 4 => "--lowvram",
+            < 8 => "--medvram",
+            _ => string.Empty
+        };
+    }
+
+    private static string GetXformersOption()
+    {
+        var gpuName = HardwareHelper.GetGpuChipName();
+        return gpuName.Contains("NVIDIA", StringComparison.OrdinalIgnoreCase) ? "--xformers" : string.Empty;
+    }
+}

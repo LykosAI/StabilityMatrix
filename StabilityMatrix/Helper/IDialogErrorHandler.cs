20c1c8553784143d5cc0ac10e2d6805510143c55
Ionite <dev@ionite.io>
2023-05-31 20:22:56 -0400

    DialogErrorHandler add support for void Tasks

--- StabilityMatrix/Helper/IDialogErrorHandler.cs
+++ StabilityMatrix/Helper/IDialogErrorHandler.cs
@@ -12,7 +12,13 @@ public interface IDialogErrorHandler
     void ShowSnackbarAsync(string message, LogLevel level = LogLevel.Error, int timeoutMilliseconds = 5000);
 
     /// <summary>
-    /// Attempt to run the given action, showing a generic error snackbar if it fails.
+    /// Attempt to run the given task, showing a generic error snackbar if it fails.
     /// </summary>
     Task<TaskResult<T>> TryAsync<T>(Task<T> task, string message, LogLevel level = LogLevel.Error, int timeoutMilliseconds = 5000);
+    
+    /// <summary>
+    /// Attempt to run the given void task, showing a generic error snackbar if it fails.
+    /// Return a TaskResult with true if the task succeeded, false if it failed.
+    /// </summary>
+    Task<TaskResult<bool>> TryAsync(Task task, string message, LogLevel level = LogLevel.Error, int timeoutMilliseconds = 5000);
 }

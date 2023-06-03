8ec2753677830bf818f2cd809d553061227ba003
Ionite <dev@ionite.io>
2023-06-02 20:43:28 -0400

    Fix checkpoint folder cards scroll

--- StabilityMatrix/CheckpointManagerPage.xaml.cs
+++ StabilityMatrix/CheckpointManagerPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Effects;
 using StabilityMatrix.ViewModels;
@@ -73,5 +74,26 @@ public partial class CheckpointManagerPage : Page
     private async void CheckpointManagerPage_OnLoaded(object sender, RoutedEventArgs e)
     {
         await viewModel.OnLoaded();
+    }
+
+    /// <summary>
+    /// Bubbles the mouse wheel event up to the parent.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void VirtualizingGridView_OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        if (e.Handled) return;
+        
+        e.Handled = true;
+        var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+        {
+            RoutedEvent = MouseWheelEvent,
+            Source = sender
+        };
+        if (((Control)sender).Parent is UIElement parent)
+        {
+            parent.RaiseEvent(eventArg);
+        }
     }
 }

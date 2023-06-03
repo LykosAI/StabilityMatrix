28bbe544a31b19ac08ee43a94c9eadb1ddd4c2ea
Ionite <dev@ionite.io>
2023-06-02 23:07:15 -0400

    Improve checkpoint index speed with concurrency

--- StabilityMatrix/ViewModels/CheckpointManagerViewModel.cs
+++ StabilityMatrix/ViewModels/CheckpointManagerViewModel.cs
@@ -1,24 +1,51 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using NLog;
 using StabilityMatrix.Models;
 
 namespace StabilityMatrix.ViewModels;
 
 public partial class CheckpointManagerViewModel : ObservableObject
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    private readonly ISharedFolders sharedFolders;
     public ObservableCollection<CheckpointFolder> CheckpointFolders { get; set; } = new();
+    
+    public CheckpointManagerViewModel(ISharedFolders sharedFolders)
+    {
+        this.sharedFolders = sharedFolders;
+    }
     
     public async Task OnLoaded()
     {
-        CheckpointFolders.Clear();
-        var folder = new CheckpointFolder
+        // Get all folders within the shared folder root
+        if (string.IsNullOrWhiteSpace(sharedFolders.SharedFoldersPath))
         {
-            Title = "Stable Diffusion",
-            DirectoryPath = @"L:\Image ML\stable-diffusion-webui\models\Stable-diffusion"
-        };
-        CheckpointFolders.Add(folder);
-        await folder.IndexAsync();
+            return;
+        }
+        var folders = Directory.GetDirectories(sharedFolders.SharedFoldersPath);
+        
+        CheckpointFolders.Clear();
+
+        // Results
+        var indexedFolders = new ConcurrentBag<CheckpointFolder>();
+        // Index all folders
+        var tasks = folders.Select(f => Task.Run(async () =>
+            {
+                var checkpointFolder = new CheckpointFolder {Title = Path.GetFileName(f), DirectoryPath = f};
+                await checkpointFolder.IndexAsync();
+                indexedFolders.Add(checkpointFolder);
+            })).ToList();
+        await Task.WhenAll(tasks);
+        // Add to observable collection by alphabetical order
+        foreach (var checkpointFolder in indexedFolders.OrderBy(f => f.Title))
+        {
+            CheckpointFolders.Add(checkpointFolder);
+        }
     }
 
     public void OnFolderCardDrop()

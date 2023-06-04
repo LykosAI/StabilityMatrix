ea29642a64b35cf03763755ec86d3fc28d676346
Ionite <dev@ionite.io>
2023-06-04 00:50:37 -0400

    Setup app info screen and license dialog

---
+++ StabilityMatrix/Models/LicenseInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace StabilityMatrix.Models;
+
+public class LicenseInfo
+{
+    public string PackageName { get; set; }
+    public string PackageVersion { get; set; }
+    public string PackageUrl { get; set; }
+    public string Copyright { get; set; }
+    public List<string> Authors { get; set; }
+    public string Description { get; set; }
+    public string LicenseUrl { get; set; }
+    public string LicenseType { get; set; }
+}

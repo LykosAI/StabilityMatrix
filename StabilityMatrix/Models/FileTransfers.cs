8efa6fff15018ed742052c39a66774c6305a09a0
Ionite <dev@ionite.io>
2023-06-04 19:21:09 -0400

    Add FileTransfers

---
+++ StabilityMatrix/Models/FileTransfers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StabilityMatrix.Models;
+
+public static class FileTransfers
+{
+    public const int BufferSize = 10240;
+    public static async Task CopyFiles(Dictionary<string, string> files, IProgress<ProgressReport>? fileProgress = default, IProgress<ProgressReport>? totalProgress = default)
+    {
+        var totalFiles = files.Count;
+        var currentFiles = 0;
+        var totalSize = Convert.ToUInt64(files.Keys.Select(x => new FileInfo(x).Length).Sum());
+        var totalRead = 0ul;
+
+        foreach(var (sourcePath, destPath) in files)
+        {
+            var totalReadForFile = 0ul;
+
+            await using var outStream = new FileStream(destPath, FileMode.Create, FileAccess.Write, FileShare.Read);
+            await using var inStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            var fileSize = (ulong) inStream.Length;
+            var fileName = Path.GetFileName(sourcePath);
+            await CopyStream(inStream , outStream, fileReadBytes =>
+            {
+                currentFiles++;
+                var lastRead = totalReadForFile;
+                totalReadForFile = Convert.ToUInt64(fileReadBytes);
+                totalRead += totalReadForFile - lastRead;
+                fileProgress?.Report(new ProgressReport(totalReadForFile, fileSize, fileName, $"{currentFiles}/{totalFiles}"));
+                totalProgress?.Report(new ProgressReport(totalRead, totalSize, fileName, $"{currentFiles}/{totalFiles}"));
+            } );
+        }
+    }
+
+    private static async Task CopyStream(Stream from, Stream to, Action<long> progress)
+    {
+        var buffer = new byte[BufferSize];
+        var totalRead = 0;
+
+        while (totalRead < from.Length)
+        {
+            var read = await from.ReadAsync(buffer.AsMemory(0, BufferSize));
+            await to.WriteAsync(buffer.AsMemory(0, read));
+            totalRead += read;
+            progress(totalRead);
+        }
+    }
+}

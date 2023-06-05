cb3c9fcc1550ce4d60f54f0b687dcdb756dba73b
Ionite <dev@ionite.io>
2023-06-05 04:19:44 -0400

    Apparently the response can be empty

--- StabilityMatrix/Models/Api/CivitModelsResponse.cs
+++ StabilityMatrix/Models/Api/CivitModelsResponse.cs
@@ -5,8 +5,8 @@ namespace StabilityMatrix.Models.Api;
 public class CivitModelsResponse
 {
     [JsonPropertyName("items")]
-    public CivitModel[] Items { get; set; }
+    public CivitModel[]? Items { get; set; }
     
     [JsonPropertyName("metadata")]
-    public CivitMetadata Metadata { get; set; }
+    public CivitMetadata? Metadata { get; set; }
 }

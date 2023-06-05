e11f3d36c37c46d92345e055cc78d6fdc51f9ccf
Ionite <dev@ionite.io>
2023-06-05 19:04:13 -0400

    Add ConnectedModelInfo, deletion progress

---
+++ StabilityMatrix/Converters/BooleanToHiddenVisibleConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace StabilityMatrix.Converters;
+
+public class BooleanToHiddenVisibleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var bValue = false;
+        if (value is bool b)
+        {
+            bValue = b;
+        }
+        else if (value is bool)
+        {
+            var tmp = (bool?) value;
+            bValue = tmp.Value;
+        }
+        return bValue ? Visibility.Visible : Visibility.Hidden;
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Visibility visibility)
+        {
+            return visibility == Visibility.Visible;
+        }
+        return false;
+    }
+}

c003587195bee21bee145a6347274c2a82ef1cca
Ionite <dev@ionite.io>
2023-06-05 21:23:24 -0400

    poofed CivitModelTypeExtensions

--- StabilityMatrix/Models/Api/CivitModelType.cs
+++ StabilityMatrix/Models/Api/CivitModelType.cs
@@ -26,23 +26,3 @@ public enum CivitModelType
     [ConvertTo<SharedFolderType>(SharedFolderType.LyCORIS)]
     LoCon
 }
-
-public static class CivitModelTypeExtensions
-{
-    public static SharedFolderType ToSharedFolderType(this CivitModelType type)
-    {
-        return type switch
-        {
-            CivitModelType.Checkpoint => SharedFolderType.StableDiffusion,
-            CivitModelType.TextualInversion => SharedFolderType.TextualInversion,
-            CivitModelType.Hypernetwork => SharedFolderType.Hypernetwork,
-            //CivitModelType.AestheticGradient => expr,
-            CivitModelType.LORA => SharedFolderType.Lora,
-            CivitModelType.Controlnet => SharedFolderType.ControlNet,
-            //CivitModelType.Poses => expr,
-            CivitModelType.Model => SharedFolderType.StableDiffusion,
-            CivitModelType.LoCon => SharedFolderType.LyCORIS,
-            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-        };
-    }
-}

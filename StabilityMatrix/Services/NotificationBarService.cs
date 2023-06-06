596021f9f8c548303fa08fdc267b13100b9be5df
Ionite <dev@ionite.io>
2023-06-06 18:55:45 -0400

    Add Sentry and SafeFireAndForget extension

--- StabilityMatrix/Services/NotificationBarService.cs
+++ StabilityMatrix/Services/NotificationBarService.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using AsyncAwaitBestPractices;
+using Wpf.Ui.Common;
 using Wpf.Ui.Contracts;
+using Wpf.Ui.Controls;
+using Wpf.Ui.Controls.IconElements;
 using Wpf.Ui.Services;
 
 namespace StabilityMatrix.Services;
@@ -13,5 +16,11 @@ public class NotificationBarService : SnackbarService, INotificationBarService
 {
     public void ShowStartupNotifications()
     {
+        Timeout = 10000;
+        var linkIcon = new SymbolIcon(SymbolRegular.Link24);
+        var snackbar = ShowAsync(
+            "Welcome to StabilityMatrix!",
+            "You can join our Discord server for support and feedback.", linkIcon, ControlAppearance.Info);
+        snackbar.SafeFireAndForget();
     }
 }

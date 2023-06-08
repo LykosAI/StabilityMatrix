54cc9cd7b09a3fca938444ef4854db085464af51
JT <mohnjiles@gmail.com>
2023-06-07 20:45:41 -0700

    fix test

--- StabilityMatrix.Tests/Helper/PackageFactoryTests.cs
+++ StabilityMatrix.Tests/Helper/PackageFactoryTests.cs
@@ -16,7 +16,7 @@ public class PackageFactoryTests
         fakeBasePackages = new List<BasePackage>
         {
             // TODO: inject mocks
-            new DankDiffusion(null, null, null)
+            new DankDiffusion(null, null, null, null)
         };
         packageFactory = new PackageFactory(fakeBasePackages);
     }

c60b9d5e573be5e7ffbe6541ba8ee5d075243762
JT <mohnjiles@gmail.com>
2023-06-26 19:47:31 -0700

    Update StabilityMatrix/ViewModels/CheckpointBrowserViewModel.cs
    Co-authored-by: Ionite <dev@ionite.io>

--- StabilityMatrix/ViewModels/CheckpointBrowserViewModel.cs
+++ StabilityMatrix/ViewModels/CheckpointBrowserViewModel.cs
@@ -333,8 +333,8 @@ public partial class CheckpointBrowserViewModel : ObservableObject
     private void UpdateResultsText()
     {
         NoResultsFound = ModelCardsView?.IsEmpty ?? true;
-        NoResultsText = ModelCards == null || ModelCards.Count == 0
-            ? "No results found"
-            : $"{ModelCards?.Count} results hidden by filters";
+        NoResultsText = ModelCards?.Count > 0
+            ? $"{ModelCards.Count} results hidden by filters"
+            : "No results found";
     }
 }

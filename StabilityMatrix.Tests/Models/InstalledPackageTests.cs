62b7b3bae90e3b6ffad494b42c80b365a3eb8396
Ionite <dev@ionite.io>
2023-06-27 01:32:12 -0400

    Add GetSubPath tests

---
+++ StabilityMatrix.Tests/Models/InstalledPackageTests.cs
@@ -0,0 +1,18 @@
+﻿using StabilityMatrix.Models;
+
+namespace StabilityMatrix.Tests.Models;
+
+[TestClass]
+public class InstalledPackageTests
+{
+    [DataTestMethod]
+    [DataRow("C:\\User\\AppData\\StabilityMatrix", "C:\\User\\Other", null)]
+    [DataRow("C:\\Data", "D:\\Data\\abc", null)]
+    [DataRow("C:\\Data", "C:\\Data\\abc", "abc")]
+    [DataRow("C:\\User\\AppData\\StabilityMatrix", "C:\\User\\AppData\\StabilityMatrix\\Packages\\abc", "Packages\\abc")]
+    public void TestGetSubPath(string relativeTo, string path, string? expected)
+    {
+        var result = InstalledPackage.GetSubPath(relativeTo, path);
+        Assert.AreEqual(expected, result);
+    }
+}

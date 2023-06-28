6403d8072f50392d3c88ca59ddb34b59951f06d1
JT <mohnjiles@gmail.com>
2023-06-27 23:51:48 -0700

    Add IsStringNullOrWhitespaceConverter

---
+++ StabilityMatrix/Converters/IsStringNullOrWhitespaceConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace StabilityMatrix.Converters;
+
+[ValueConversion(typeof(string), typeof(bool))]
+public class IsStringNullOrWhitespaceConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string strValue)
+        {
+            return string.IsNullOrWhiteSpace(strValue);
+        }
+
+        throw new InvalidOperationException("Cannot convert non-string value");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}

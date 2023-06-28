53a61d77ba5fca69551fa876d7be2f408c2c2b6b
Ionite <dev@ionite.io>
2023-06-28 17:43:35 -0400

    Add file interfaces classes

---
+++ StabilityMatrix/Models/FileInterfaces/DirectoryPath.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StabilityMatrix.Models.FileInterfaces;
+
+public class DirectoryPath : FileSystemPath, IPathObject
+{
+    private DirectoryInfo? _info;
+    // ReSharper disable once MemberCanBePrivate.Global
+    public DirectoryInfo Info => _info ??= new DirectoryInfo(FullPath);
+
+    public bool IsSymbolicLink
+    {
+        get
+        {
+            Info.Refresh();
+            return Info.Attributes.HasFlag(FileAttributes.ReparsePoint);
+        }
+    }
+    
+    public bool Exists => Info.Exists;
+
+    public DirectoryPath(string path) : base(path)
+    {
+    }
+    
+    public DirectoryPath(FileSystemPath path) : base(path)
+    {
+    }
+    
+    public DirectoryPath(params string[] paths) : base(paths)
+    {
+    }
+
+    public long GetSize()
+    {
+        ulong size = 1 + 2;
+        Info.Refresh();
+        return Info.EnumerateFiles("*", SearchOption.AllDirectories)
+            .Sum(file => file.Length);
+    }
+    
+    public long GetSize(bool includeSymbolicLinks)
+    {
+        if (includeSymbolicLinks) return GetSize();
+        
+        Info.Refresh();
+        var files = Info.GetFiles()
+            .Where(file => !file.Attributes.HasFlag(FileAttributes.ReparsePoint))
+            .Sum(file => file.Length);
+        var subDirs = Info.GetDirectories()
+            .Where(dir => !dir.Attributes.HasFlag(FileAttributes.ReparsePoint))
+            .Sum(dir => dir.EnumerateFiles("*", SearchOption.AllDirectories).Sum(file => file.Length));
+        return files + subDirs;
+    }
+
+    public Task<long> GetSizeAsync(bool includeSymbolicLinks)
+    {
+        return Task.Run(() => GetSize(includeSymbolicLinks));
+    }
+    
+    /// <summary> Creates the directory. </summary>
+    public void Create() => Directory.CreateDirectory(FullPath);
+    
+    /// <summary> Deletes the directory </summary>
+    public void Delete() => Directory.Delete(FullPath);
+    
+    /// <summary> Deletes the directory. </summary>
+    public void Delete(bool recursive) => Directory.Delete(FullPath, recursive);
+
+    /// <summary> Deletes the directory asynchronously. </summary>
+    public Task DeleteAsync(bool recursive) => Task.Run(() => Delete(recursive));
+    
+    // Implicit conversions to and from string
+    public static implicit operator string(DirectoryPath path) => path.FullPath;
+    public static implicit operator DirectoryPath(string path) => new(path);
+}
---
+++ StabilityMatrix/Models/FileInterfaces/FileSystemPath.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace StabilityMatrix.Models.FileInterfaces;
+
+public class FileSystemPath
+{
+    public string FullPath { get; }
+
+    protected FileSystemPath(string path)
+    {
+        FullPath = path;
+    }
+    
+    protected FileSystemPath(FileSystemPath path) : this(path.FullPath)
+    {
+    }
+
+    protected FileSystemPath(params string[] paths) : this(Path.Combine(paths))
+    {
+    }
+    
+    // Add operators to join other paths or strings
+    public static FileSystemPath operator +(FileSystemPath path, FileSystemPath other) => new(Path.Combine(path.FullPath, other.FullPath));
+    public static FileSystemPath operator +(FileSystemPath path, string other) => new(Path.Combine(path.FullPath, other));
+    
+    // Adding directory or file results in their types
+    public static DirectoryPath operator +(FileSystemPath path, DirectoryPath other) => new(Path.Combine(path.FullPath, other.FullPath));
+    public static FilePath operator +(FileSystemPath path, FilePath other) => new(Path.Combine(path.FullPath, other.FullPath));
+
+    // Implicit conversions to and from string
+    public static implicit operator string(FileSystemPath path) => path.FullPath;
+    public static implicit operator FileSystemPath(string path) => new(path);
+}

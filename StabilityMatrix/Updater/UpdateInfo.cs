f064697493e09170f20ab0f22b2bf78d01d32b5e
Ionite <dev@ionite.io>
2023-07-01 03:41:53 -0400

    Add hash_blake3 field to updateinfo

--- StabilityMatrix/Updater/UpdateInfo.cs
+++ StabilityMatrix/Updater/UpdateInfo.cs
@@ -13,15 +13,22 @@ public record UpdateInfo(
     [property: JsonPropertyName("channel")]
     UpdateChannel Channel,
     
+    [property: JsonPropertyName("type")] 
+    UpdateType Type,
+    
     [property: JsonPropertyName("url")] 
     string DownloadUrl,
     
     [property: JsonPropertyName("changelog")]
     string ChangelogUrl,
     
-    [property: JsonPropertyName("signature")]
-    string Signature,
+    // Blake3 hash of the file
+    [property: JsonPropertyName("hash_blake3")]
+    string HashBlake3,
     
-    [property: JsonPropertyName("type")] 
-    UpdateType Type
+    // ED25519 signature of the semicolon seperated string:
+    // "version + releaseDate + channel + type + url + changelog + hash_blake3"
+    // verifiable using our stored public key
+    [property: JsonPropertyName("signature")]
+    string Signature
 );

ddb22227189c697ea93e4008bdb14b0539c6fe3f
Ionite <dev@ionite.io>
2023-07-03 16:11:41 -0400

    Improve model downloader error handling

--- StabilityMatrix/ViewModels/CheckpointBrowserCardViewModel.cs
+++ StabilityMatrix/ViewModels/CheckpointBrowserCardViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,17 +6,21 @@ using System.Windows;
 using System.Windows.Media.Imaging;
 using AsyncAwaitBestPractices;
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.Extensions.Logging;
+using NLog;
+using Refit;
 using StabilityMatrix.Extensions;
 using StabilityMatrix.Helper;
 using StabilityMatrix.Models;
 using StabilityMatrix.Models.Api;
+using StabilityMatrix.Models.Progress;
 using StabilityMatrix.Services;
+using Wpf.Ui.Controls;
 
 namespace StabilityMatrix.ViewModels;
 
 public partial class CheckpointBrowserCardViewModel : ProgressViewModel
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
     private readonly IDownloadService downloadService;
     private readonly ISnackbarService snackbarService;
     private readonly ISettingsManager settingsManager;
@@ -72,32 +75,50 @@ public partial class CheckpointBrowserCardViewModel : ProgressViewModel
     {
         Text = "Downloading...";
         
+        // Get latest version
         if (!(model.ModelVersions?.Count > 0))
         {
             snackbarService.ShowSnackbarAsync(
                 "This model has no versions available for download",
-                "Download failed", LogLevel.Warning).SafeFireAndForget();
+                "Model has no versions available", ControlAppearance.Caution).SafeFireAndForget();
             return;
         }
-
-        var latestVersion = model.ModelVersions[0];
-        var latestModelFile = latestVersion.Files[0];
-        var fileExpectedSha256 = latestModelFile.Hashes.SHA256;
+        var modelVersion = model.ModelVersions[0];
+        
+        // Get latest version file
+        if (!(modelVersion.Files?.Count > 0))
+        {
+            snackbarService.ShowSnackbarAsync(
+                "This model has no files available for download",
+                "Model has no files available", ControlAppearance.Caution).SafeFireAndForget();
+            return;
+        }
+        var modelFile = modelVersion.Files[0];
         
         var downloadFolder = Path.Combine(settingsManager.ModelsDirectory,
             model.Type.ConvertTo<SharedFolderType>().GetStringValue());
         // Folders might be missing if user didn't install any packages yet
         Directory.CreateDirectory(downloadFolder);
-        var downloadPath = Path.GetFullPath(Path.Combine(downloadFolder, latestModelFile.Name));
-
-        var downloadProgress = new Progress<ProgressReport>(progress =>
+        var downloadPath = Path.GetFullPath(Path.Combine(downloadFolder, modelFile.Name));
+        
+        // Do the download
+        var downloadTask = downloadService.DownloadToFileAsync(modelFile.DownloadUrl, downloadPath, 
+            new Progress<ProgressReport>(report =>
+            {
+                Value = report.Percentage;
+                Text = $"Downloading... {report.Percentage}%";
+            }));
+        var downloadResult = await snackbarService.TryAsync(downloadTask, "Could not download file");
+        
+        // For exceptions other than ApiException or TaskCanceledException, log error
+        if (downloadResult is {Exception: not null and not (ApiException or TaskCanceledException)})
         {
-            Value = progress.Percentage;
-            Text = $"Importing... {progress.Percentage}%";
-        });
-        await downloadService.DownloadToFileAsync(latestModelFile.DownloadUrl, downloadPath, progress: downloadProgress);
+            Logger.Error(downloadResult.Exception, "Unexpected error during model download");
+            return;
+        }
         
         // When sha256 is available, validate the downloaded file
+        var fileExpectedSha256 = modelFile.Hashes.SHA256;
         if (!string.IsNullOrEmpty(fileExpectedSha256))
         {
             var hashProgress = new Progress<ProgressReport>(progress =>
@@ -112,24 +133,24 @@ public partial class CheckpointBrowserCardViewModel : ProgressViewModel
                 DelayedClearProgress(TimeSpan.FromMilliseconds(800));
                 snackbarService.ShowSnackbarAsync(
                     "This may be caused by network or server issues from CivitAI, please try again in a few minutes.",
-                    "Download failed hash validation", LogLevel.Warning).SafeFireAndForget();
+                    "Download failed hash validation").SafeFireAndForget();
                 return;
             }
             snackbarService.ShowSnackbarAsync($"{model.Type} {model.Name} imported successfully!",
-                "Import complete", LogLevel.Trace).SafeFireAndForget();
+                "Import complete", ControlAppearance.Info).SafeFireAndForget();
         }
         
         IsIndeterminate = true;
         
         // Save connected model info
-        var modelFileName = Path.GetFileNameWithoutExtension(latestModelFile.Name);
-        var modelInfo = new ConnectedModelInfo(CivitModel, latestVersion, latestModelFile, DateTime.UtcNow);
+        var modelFileName = Path.GetFileNameWithoutExtension(modelFile.Name);
+        var modelInfo = new ConnectedModelInfo(CivitModel, modelVersion, modelFile, DateTime.UtcNow);
         await modelInfo.SaveJsonToDirectory(downloadFolder, modelFileName);
         
         // If available, save a model image
-        if (latestVersion.Images != null && latestVersion.Images.Any())
+        if (modelVersion.Images != null && modelVersion.Images.Any())
         {
-            var image = latestVersion.Images[0];
+            var image = modelVersion.Images[0];
             var imageExtension = Path.GetExtension(image.Url).TrimStart('.');
             if (imageExtension is "jpg" or "jpeg" or "png")
             {

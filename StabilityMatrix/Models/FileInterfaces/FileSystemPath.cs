eaddd06063ce7ec79d2728bac76aced49b8f9593
Ionite <dev@ionite.io>
2023-07-03 17:03:41 -0400

    Implement IEquatable for FileSystemPath

--- StabilityMatrix/Models/FileInterfaces/FileSystemPath.cs
+++ StabilityMatrix/Models/FileInterfaces/FileSystemPath.cs
@@ -1,8 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace StabilityMatrix.Models.FileInterfaces;
 
-public class FileSystemPath
+public class FileSystemPath : IEquatable<FileSystemPath>, IEquatable<string>
 {
     public string FullPath { get; }
 
@@ -17,5 +18,33 @@ public class FileSystemPath
 
     protected FileSystemPath(params string[] paths) : this(Path.Combine(paths))
     {
+    }
+
+    public bool Equals(FileSystemPath? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return FullPath == other.FullPath;
+    }
+    
+    public bool Equals(string? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        return string.Equals(FullPath, other);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj switch
+        {
+            FileSystemPath path => Equals(path),
+            string path => Equals(path),
+            _ => false
+        };
+    }
+
+    public override int GetHashCode()
+    {
+        return FullPath.GetHashCode();
     }
 }

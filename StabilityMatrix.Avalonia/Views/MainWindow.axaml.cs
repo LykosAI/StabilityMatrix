3a8f2baea7dd59ad9f06bf4e28954388537dca88
Ionite <dev@ionite.io>
2023-07-07 00:25:06 -0400

    Update to AppWindow, add title bar content

--- StabilityMatrix.Avalonia/Views/MainWindow.axaml.cs
+++ StabilityMatrix.Avalonia/Views/MainWindow.axaml.cs
@@ -1,12 +1,22 @@
+using System.Runtime.InteropServices;
+using Avalonia;
 using Avalonia.Controls;
+using FluentAvalonia.UI.Windowing;
 using StabilityMatrix.Avalonia.ViewModels;
 
 namespace StabilityMatrix.Avalonia.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : AppWindow
 {
     public MainWindow()
     {
         InitializeComponent();
+        
+#if DEBUG
+        this.AttachDevTools();
+#endif
+
+        TitleBar.ExtendsContentIntoTitleBar = true;
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
     }
 }

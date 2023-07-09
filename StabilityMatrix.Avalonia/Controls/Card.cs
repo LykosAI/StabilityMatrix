10d878a43fd4e6e635d9abca191cfe052449c4dd
JT <mohnjiles@gmail.com>
2023-07-08 17:41:20 -0700

    Added Card control

---
+++ StabilityMatrix.Avalonia/Controls/Card.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Styling;
+
+namespace StabilityMatrix.Avalonia.Controls;
+
+public class Card : Expander
+{
+    public Card()
+    {
+        // Expander /template/ ToggleButton#PART_toggle
+        var customStyle = new Style(x =>
+            x.OfType<Expander>().Template().OfType<ToggleButton>().Name("PART_toggle"));
+        
+        customStyle.Setters.Add(new Setter
+        {
+            Property = IsVisibleProperty,
+            Value = false
+        });
+        
+        Styles.Add(customStyle);
+    }
+}

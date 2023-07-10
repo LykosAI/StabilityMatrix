496258e3d5cce53668af6c1724a1050ade2f550f
Ionite <dev@ionite.io>
2023-07-10 00:13:39 -0400

    Add mica effect on win 11

--- StabilityMatrix.Avalonia/Views/MainWindow.axaml.cs
+++ StabilityMatrix.Avalonia/Views/MainWindow.axaml.cs
@@ -1,5 +1,12 @@
+using System;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Media.Immutable;
+using Avalonia.Styling;
+using FluentAvalonia.Styling;
+using FluentAvalonia.UI.Media;
 using FluentAvalonia.UI.Windowing;
 using StabilityMatrix.Avalonia.Controls;
 using StabilityMatrix.Avalonia.Services;
@@ -22,9 +29,64 @@ public partial class MainWindow : AppWindowBase
         TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
     }
 
+    protected override void OnOpened(EventArgs e)
+    {
+        base.OnOpened(e);
+
+        Application.Current!.ActualThemeVariantChanged += OnActualThemeVariantChanged;
+        
+        var theme = ActualThemeVariant;
+        if (IsWindows11 && theme != FluentAvaloniaTheme.HighContrastTheme)
+        {
+            TryEnableMicaEffect();
+        }
+    }
+    
     public override void OnLoaded(object? sender, RoutedEventArgs e)
     {
         base.OnLoaded(sender, e);
         NotificationService?.Initialize(this);
     }
+    
+    private void OnActualThemeVariantChanged(object? sender, EventArgs e)
+    {
+        if (IsWindows11)
+        {
+            if (ActualThemeVariant != FluentAvaloniaTheme.HighContrastTheme)
+            {
+                TryEnableMicaEffect();
+            }
+            else
+            {
+                ClearValue(BackgroundProperty);
+                ClearValue(TransparencyBackgroundFallbackProperty);
+            }
+        }
+    }
+    
+    private void TryEnableMicaEffect()
+    {
+        TransparencyBackgroundFallback = Brushes.Transparent;
+        TransparencyLevelHint = new[] { WindowTransparencyLevel.Mica, WindowTransparencyLevel.Blur };
+        
+        if (ActualThemeVariant == ThemeVariant.Dark)
+        {
+            var color = this.TryFindResource("SolidBackgroundFillColorBase",
+                ThemeVariant.Dark, out var value) ? (Color2)(Color)value! : new Color2(32, 32, 32);
+
+            color = color.LightenPercent(-0.8f);
+
+            Background = new ImmutableSolidColorBrush(color, 0.9);
+        }
+        else if (ActualThemeVariant == ThemeVariant.Light)
+        {
+            // Similar effect here
+            var color = this.TryFindResource("SolidBackgroundFillColorBase",
+                ThemeVariant.Light, out var value) ? (Color2)(Color)value! : new Color2(243, 243, 243);
+
+            color = color.LightenPercent(0.5f);
+
+            Background = new ImmutableSolidColorBrush(color, 0.9);
+        }
+    } 
 }

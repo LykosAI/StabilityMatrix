f24d6f05fd1495edc2cbfd4e9aff05793c574215
Ionite <dev@ionite.io>
2023-07-11 03:56:20 -0400

    Add platform compatibility static class

---
+++ StabilityMatrix.Core/Helper/Compat.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using StabilityMatrix.Core.Models.FileInterfaces;
+
+namespace StabilityMatrix.Core.Helper;
+
+/// <summary>
+/// Compatibility layer for checks and file paths on different platforms.
+/// </summary>
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+public static class Compat
+{
+    private const string AppName = "StabilityMatrix";
+    
+    // OS Platform
+    public static PlatformKind Platform { get; }
+    public static bool IsWindows => Platform.HasFlag(PlatformKind.Windows);
+    public static bool IsLinux => Platform.HasFlag(PlatformKind.Linux);
+    public static bool IsMacOS => Platform.HasFlag(PlatformKind.MacOS);
+    public static bool IsUnix => Platform.HasFlag(PlatformKind.Unix);
+    
+    // Paths
+    
+    /// <summary>
+    /// AppData directory path. On Windows this is %AppData%, on Linux and MacOS this is ~/.config
+    /// </summary>
+    public static DirectoryPath AppData { get; }
+    
+    /// <summary>
+    /// AppData + AppName (e.g. %AppData%\StabilityMatrix)
+    /// </summary>
+    public static DirectoryPath AppDataHome { get; }
+
+    /// <summary>
+    /// Current directory the app is in.
+    /// </summary>
+    public static DirectoryPath AppCurrentDir { get; }
+    
+    static Compat()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Platform = PlatformKind.Windows;
+        }
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Platform = PlatformKind.MacOS | PlatformKind.Unix;
+        }
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Platform = PlatformKind.Linux | PlatformKind.Unix;
+        }
+        
+        AppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        AppDataHome = AppData + AppName;
+        
+        AppCurrentDir = Platform switch 
+        {
+            PlatformKind.Windows => AppContext.BaseDirectory,
+            PlatformKind.Linux => Environment.GetEnvironmentVariable("APPIMAGE") ??
+                                  Environment.CurrentDirectory,
+            _ => throw new PlatformNotSupportedException($"{Platform}")
+        };
+    }
+}
---
+++ StabilityMatrix.Core/Helper/PlatformKind.cs
@@ -0,0 +1,11 @@
+﻿namespace StabilityMatrix.Core.Helper;
+
+[Flags]
+public enum PlatformKind
+{
+    Unknown = 0,
+    Windows = 1 << 0,
+    Unix = 1 << 1,
+    Linux = Unix | 1 << 2,
+    MacOS = Unix | 1 << 3,
+}

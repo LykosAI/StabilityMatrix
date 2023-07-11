b3733262e4dc782074fef7c61ff24cc67c52581a
Ionite <dev@ionite.io>
2023-07-11 17:16:41 -0400

    Add tests to ensure DesignData ctor runs

---
+++ StabilityMatrix.Tests/Avalonia/DesignDataTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using StabilityMatrix.Avalonia.DesignData;
+
+namespace StabilityMatrix.Tests.Avalonia;
+
+[TestClass]
+public class DesignDataTests
+{
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        // invoke static ctor by property access
+        Assert.IsNotNull(StabilityMatrix.Avalonia.DesignData.DesignData.LaunchPageViewModel);
+    }
+    
+    // Return all properties
+    public static IEnumerable<object[]> DesignDataProperties => 
+        typeof(StabilityMatrix.Avalonia.DesignData.DesignData).GetProperties()
+            .Select(p => new object[] { p });
+    
+    [TestMethod]
+    [DynamicData(nameof(DesignDataProperties))]
+    public void Property_ShouldBeNotNull(PropertyInfo property)
+    {
+        var value = property.GetValue(null);
+        Assert.IsNotNull(value);
+    }
+}

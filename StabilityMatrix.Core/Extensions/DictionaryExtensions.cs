ec9f818e1b0a7c74ff9b871da15b990ab3344496
Ionite <dev@ionite.io>
2023-07-28 15:17:52 -0400

    Add Update extension for Dictionary

---
+++ StabilityMatrix.Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace StabilityMatrix.Core.Extensions;
+
+public static class DictionaryExtensions
+{
+    /// <summary>
+    /// Adds all items from another dictionary to this dictionary.
+    /// </summary>
+    public static void Update<TKey, TValue>(
+        this Dictionary<TKey, TValue> source,
+        IReadOnlyDictionary<TKey, TValue> collection
+    )
+        where TKey : notnull
+    {
+        foreach (var item in collection)
+        {
+            source[item.Key] = item.Value;
+        }
+    }
+}

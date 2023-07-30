0b90944d50b776dbb3543e0325a2a68d58745be0
Ionite <dev@ionite.io>
2023-07-30 19:59:16 -0400

    Add SeedCardViewModel

---
+++ StabilityMatrix.Avalonia/ViewModels/Inference/SeedCardViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using StabilityMatrix.Avalonia.Controls;
+using StabilityMatrix.Core.Attributes;
+
+namespace StabilityMatrix.Avalonia.ViewModels.Inference;
+
+[View(typeof(SeedCard))]
+public partial class SeedCardViewModel : ViewModelBase
+{
+    [ObservableProperty, NotifyPropertyChangedFor(nameof(RandomizeButtonToolTip))]
+    private bool isRandomizeEnabled;
+    
+    [ObservableProperty] 
+    private long seed;
+
+    public string RandomizeButtonToolTip => IsRandomizeEnabled 
+        ? "Seed is locked" 
+        : "Randomizing Seed on each run";
+    
+    [RelayCommand]
+    public void GenerateNewSeed()
+    {
+        Seed = Random.Shared.NextInt64();
+    }
+}

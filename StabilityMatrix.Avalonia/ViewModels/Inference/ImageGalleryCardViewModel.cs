5783dcfeef859066fb816a08b319e10ecd568943
Ionite <dev@ionite.io>
2023-08-05 02:40:34 -0400

    Add image copy to clipboard flyout

--- StabilityMatrix.Avalonia/ViewModels/Inference/ImageGalleryCardViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Inference/ImageGalleryCardViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using Avalonia.Collections;
 using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NLog;
 using StabilityMatrix.Avalonia.Controls;
+using StabilityMatrix.Avalonia.Helpers;
 using StabilityMatrix.Core.Attributes;
 
 namespace StabilityMatrix.Avalonia.ViewModels.Inference;
@@ -9,10 +13,24 @@ namespace StabilityMatrix.Avalonia.ViewModels.Inference;
 [View(typeof(ImageGalleryCard))]
 public partial class ImageGalleryCardViewModel : ViewModelBase
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    
     [ObservableProperty] private bool isPreviewOverlayEnabled;
 
     [ObservableProperty] private IImage? previewImage;
 
     [ObservableProperty] private AvaloniaList<string> imageSources = new();
     [ObservableProperty] private string? selectedImage;
+
+    [RelayCommand]
+    private void FlyoutCopy(IImage? image)
+    {
+        if (image is null)
+        {
+            Logger.Trace("FlyoutCopy: image is null");
+            return;
+        }
+        Logger.Trace($"FlyoutCopy is copying {image}");
+        WindowsClipboard.SetBitmap((Bitmap) image);
+    }
 }

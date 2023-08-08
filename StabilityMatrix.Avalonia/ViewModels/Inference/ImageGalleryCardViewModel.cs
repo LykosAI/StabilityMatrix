39cb652832a7757b9a8820e65154b0f0e5834761
Ionite <dev@ionite.io>
2023-08-07 22:44:44 -0400

    Fix gallery button enable state

--- StabilityMatrix.Avalonia/ViewModels/Inference/ImageGalleryCardViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Inference/ImageGalleryCardViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Avalonia.Collections;
 using Avalonia.Media;
@@ -31,15 +33,44 @@ public partial class ImageGalleryCardViewModel : ViewModelBase
     [ObservableProperty]
     private ImageSource? selectedImage;
 
-    [
-        ObservableProperty,
-        NotifyPropertyChangedFor(nameof(CanNavigateBack), nameof(CanNavigateForward))
-    ]
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(CanNavigateBack), nameof(CanNavigateForward))]
     private int selectedImageIndex;
 
     public bool CanNavigateBack => SelectedImageIndex > 0;
     public bool CanNavigateForward => SelectedImageIndex < ImageSources.Count - 1;
 
+    public ImageGalleryCardViewModel()
+    {
+        ImageSources.CollectionChanged += OnImageSourcesItemsChanged;
+    }
+    
+    private void OnImageSourcesItemsChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (sender is AvaloniaList<ImageSource> sources)
+        {
+            if (e.Action is NotifyCollectionChangedAction.Add or NotifyCollectionChangedAction.Remove
+                or NotifyCollectionChangedAction.Reset)
+            {
+                if (sources.Count == 0)
+                {
+                    SelectedImageIndex = -1;
+                }
+                else if (SelectedImageIndex == -1)
+                {
+                    SelectedImageIndex = 0;
+                }
+                // Clamp the selected index to the new range
+                else
+                {
+                    SelectedImageIndex = Math.Clamp(SelectedImageIndex, 0, sources.Count - 1);
+                }
+                OnPropertyChanged(nameof(CanNavigateBack));
+                OnPropertyChanged(nameof(CanNavigateForward));
+            }
+        }
+    }
+    
     [RelayCommand]
     // ReSharper disable once UnusedMember.Local
     private async Task FlyoutCopy(IImage? image)

a85007b07a76cb4c727d42a333b02a574d993144
Ionite <dev@ionite.io>
2023-08-09 15:50:26 -0400

    Add ComfyUpscalerType enum

---
+++ StabilityMatrix.Core/Models/Api/Comfy/ComfyUpscalerType.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using StabilityMatrix.Core.Converters.Json;
+
+namespace StabilityMatrix.Core.Models.Api.Comfy;
+
+[JsonConverter(typeof(DefaultUnknownEnumConverter<ComfyUpscalerType>))]
+public enum ComfyUpscalerType
+{
+    Unknown,
+    Latent,
+    ESRGAN
+}

77b512a598c7f97854ad8c36a488855a4fd36895
Ionite <dev@ionite.io>
2023-08-11 03:00:58 -0400

    Add override serialization for Prompt Card

--- StabilityMatrix.Avalonia/ViewModels/Inference/PromptCardViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Inference/PromptCardViewModel.cs
@@ -1,6 +1,8 @@
-﻿using AvaloniaEdit.Document;
+﻿using System.Text.Json.Nodes;
+using AvaloniaEdit.Document;
 using CommunityToolkit.Mvvm.ComponentModel;
 using StabilityMatrix.Avalonia.Controls;
+using StabilityMatrix.Avalonia.Models.Inference;
 using StabilityMatrix.Core.Attributes;
 
 namespace StabilityMatrix.Avalonia.ViewModels.Inference;
@@ -14,4 +16,23 @@ public partial class PromptCardViewModel : LoadableViewModelBase
     [ObservableProperty] private int editorFontSize = 14;
     
     [ObservableProperty] private string editorFontFamily = "Consolas";
+
+    /// <inheritdoc />
+    public override JsonObject SaveStateToJsonObject()
+    {
+        return SerializeModel(new PromptCardModel
+        {
+            Prompt = PromptDocument.Text,
+            NegativePrompt = NegativePromptDocument.Text
+        });
+    }
+
+    /// <inheritdoc />
+    public override void LoadStateFromJsonObject(JsonObject state)
+    {
+        var model = DeserializeModel<PromptCardModel>(state);
+
+        PromptDocument.Text = model.Prompt ?? "";
+        NegativePromptDocument.Text = model.NegativePrompt ?? "";
+    }
 }

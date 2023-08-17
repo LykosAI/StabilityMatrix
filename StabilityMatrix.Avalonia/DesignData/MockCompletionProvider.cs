5368371b426c1408f6128a64901b4f5fb471adb1
Ionite <dev@ionite.io>
2023-08-17 17:25:38 -0400

    Add Mock completion provider

---
+++ StabilityMatrix.Avalonia/DesignData/MockCompletionProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using StabilityMatrix.Avalonia.Controls.CodeCompletion;
+using StabilityMatrix.Avalonia.Models.TagCompletion;
+using StabilityMatrix.Core.Models.FileInterfaces;
+
+namespace StabilityMatrix.Avalonia.DesignData;
+
+public class MockCompletionProvider : ICompletionProvider
+{
+    /// <inheritdoc />
+    public bool IsLoaded => false;
+
+    /// <inheritdoc />
+    public Task LoadFromFile(FilePath path, bool recreate = false)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public void BackgroundLoadFromFile(FilePath path, bool recreate = false)
+    {
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<ICompletionData> GetCompletions(string searchTerm, int itemsCount, bool suggest)
+    {
+        return Array.Empty<ICompletionData>();
+    }
+}

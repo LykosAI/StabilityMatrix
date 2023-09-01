5a0cf9d510809844fa1d48c8eab6edd7b49570db
Ionite <dev@ionite.io>
2023-09-01 13:42:54 -0400

    Add disabled style for Cards

--- StabilityMatrix.Avalonia/Controls/Card.cs
+++ StabilityMatrix.Avalonia/Controls/Card.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 
 namespace StabilityMatrix.Avalonia.Controls;
 
@@ -7,9 +9,45 @@ public class Card : ContentControl
 {
     protected override Type StyleKeyOverride => typeof(Card);
 
+    // ReSharper disable MemberCanBePrivate.Global
+    public static readonly StyledProperty<bool> IsCardVisualsEnabledProperty =
+        AvaloniaProperty.Register<Card, bool>("IsCardVisualsEnabled", true);
+
+    /// <summary>
+    /// Whether to show card visuals.
+    /// When false, the card will have a padding of 0 and be transparent.
+    /// </summary>
+    public bool IsCardVisualsEnabled
+    {
+        get => GetValue(IsCardVisualsEnabledProperty);
+        set => SetValue(IsCardVisualsEnabledProperty, value);
+    }
+
+    // ReSharper restore MemberCanBePrivate.Global
+
     public Card()
     {
         MinHeight = 8;
         MinWidth = 8;
+    }
+
+    /// <inheritdoc />
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        // When IsCardVisualsEnabled is false, add the disabled pseudo class
+        if (change.Property == IsCardVisualsEnabledProperty)
+        {
+            PseudoClasses.Set("disabled", !change.GetNewValue<bool>());
+        }
+    }
+
+    /// <inheritdoc />
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        PseudoClasses.Set("disabled", !IsCardVisualsEnabled);
     }
 }

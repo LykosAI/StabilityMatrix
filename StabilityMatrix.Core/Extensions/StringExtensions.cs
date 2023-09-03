e76ef3508e0b29dd39db8cf45c9a314a9dabe9c3
Ionite <dev@ionite.io>
2023-09-03 18:45:56 -0400

    Add StripStart string extension method

--- StabilityMatrix.Core/Extensions/StringExtensions.cs
+++ StabilityMatrix.Core/Extensions/StringExtensions.cs
@@ -4,24 +4,26 @@ namespace StabilityMatrix.Core.Extensions;
 
 public static class StringExtensions
 {
-    private static string EncodeNonAsciiCharacters(string value) {
+    private static string EncodeNonAsciiCharacters(string value)
+    {
         var sb = new StringBuilder();
-        foreach (var c in value) 
+        foreach (var c in value)
         {
             // If not ascii or not printable
             if (c > 127 || c < 32)
             {
                 // This character is too big for ASCII
-                var encodedValue = "\\u" + ((int) c).ToString("x4");
+                var encodedValue = "\\u" + ((int)c).ToString("x4");
                 sb.Append(encodedValue);
             }
-            else {
+            else
+            {
                 sb.Append(c);
             }
         }
         return sb.ToString();
     }
-    
+
     /// <summary>
     /// Converts string to repr
     /// </summary>
@@ -35,22 +37,24 @@ public static class StringExtensions
         writer.Write("'");
         foreach (var ch in str)
         {
-            writer.Write(ch switch
-            {
-                '\0' => "\\0",
-                '\n' => "\\n",
-                '\r' => "\\r",
-                '\t' => "\\t",
-                // Non ascii
-                _ when ch > 127 || ch < 32 => $"\\u{(int) ch:x4}",
-                _ => ch.ToString()
-            });
+            writer.Write(
+                ch switch
+                {
+                    '\0' => "\\0",
+                    '\n' => "\\n",
+                    '\r' => "\\r",
+                    '\t' => "\\t",
+                    // Non ascii
+                    _ when ch > 127 || ch < 32 => $"\\u{(int)ch:x4}",
+                    _ => ch.ToString()
+                }
+            );
         }
         writer.Write("'");
-        
+
         return writer.ToString();
     }
-    
+
     /// <summary>
     /// Counts continuous sequence of a character
     /// from the start of the string
@@ -70,5 +74,14 @@ public static class StringExtensions
             }
         }
         return count;
+    }
+
+    /// <summary>
+    /// Strips the substring from the start of the string
+    /// </summary>
+    public static string StripStart(this string str, string subString)
+    {
+        var index = str.IndexOf(subString, StringComparison.Ordinal);
+        return index < 0 ? str : str.Remove(index, subString.Length);
     }
 }

1ac441e794a2195cdeb3cd072e9739dcf15371ef
Ionite <dev@ionite.io>
2023-09-18 16:12:48 -0400

    Move editor flyouts to shared resource dictionary

---
+++ StabilityMatrix.Avalonia/Controls/EditorCommands.cs
@@ -0,0 +1,16 @@
+﻿using AvaloniaEdit;
+using CommunityToolkit.Mvvm.Input;
+
+namespace StabilityMatrix.Avalonia.Controls;
+
+public static class EditorCommands
+{
+    public static RelayCommand<TextEditor> CopyCommand { get; } =
+        new(editor => editor?.Copy(), editor => editor?.CanCopy ?? false);
+
+    public static RelayCommand<TextEditor> CutCommand { get; } =
+        new(editor => editor?.Cut(), editor => editor?.CanCut ?? false);
+
+    public static RelayCommand<TextEditor> PasteCommand { get; } =
+        new(editor => editor?.Paste(), editor => editor?.CanPaste ?? false);
+}

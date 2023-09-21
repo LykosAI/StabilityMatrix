bb5c0299958f4ce6090c003a1db661bfefc32c7c
Ionite <dev@ionite.io>
2023-09-20 22:30:42 -0400

    Add more default remote upscalers

--- StabilityMatrix.Core/Models/Api/Comfy/ComfyUpscaler.cs
+++ StabilityMatrix.Core/Models/Api/Comfy/ComfyUpscaler.cs
@@ -142,6 +142,91 @@ public readonly record struct ComfyUpscaler(string Name, ComfyUpscalerType Type)
                     LicenseUrl = new Uri(HuggingFaceRoot, "RealESRGAN/LICENSE.txt"),
                     ContextType = SharedFolderType.RealESRGAN
                 }
+            },
+            new ComfyUpscaler("RealESRGAN_x4plus_anime_6B.pth", ComfyUpscalerType.DownloadableModel)
+            {
+                DownloadableResource = new RemoteResource(
+                    new Uri(
+                        HuggingFaceRoot,
+                        "/resolve/28a279ec60b5c47c9f39381dcaa997b9402ac09d/RealESRGAN/RealESRGAN_x4plus_anime_6B.pth"
+                    ),
+                    "f872d837d3c90ed2e05227bed711af5671a6fd1c9f7d7e91c911a61f155e99da"
+                )
+                {
+                    InfoUrl = new Uri("https://github.com/xinntao/ESRGAN"),
+                    Author = "xinntao",
+                    LicenseType = "Apache 2.0",
+                    LicenseUrl = new Uri(HuggingFaceRoot, "RealESRGAN/LICENSE.txt"),
+                    ContextType = SharedFolderType.RealESRGAN
+                }
+            },
+            new ComfyUpscaler("SwinIR_4x.pth", ComfyUpscalerType.DownloadableModel)
+            {
+                DownloadableResource = new RemoteResource(
+                    new Uri(
+                        HuggingFaceRoot,
+                        "/resolve/4f813cc283de5cd66fe253bc05e66ca76bf68c51/SwinIR/SwinIR_4x.pth"
+                    ),
+                    "99adfa91350a84c99e946c1eb3d8fce34bc28f57d807b09dc8fe40a316328c0a"
+                )
+                {
+                    InfoUrl = new Uri("https://github.com/JingyunLiang/SwinIR"),
+                    Author = "JingyunLiang",
+                    LicenseType = "Apache 2.0",
+                    LicenseUrl = new Uri(HuggingFaceRoot, "SwinIR/LICENSE.txt"),
+                    ContextType = SharedFolderType.SwinIR
+                }
+            },
+            new ComfyUpscaler("4x-UltraMix_Smooth.pth", ComfyUpscalerType.DownloadableModel)
+            {
+                DownloadableResource = new RemoteResource(
+                    new Uri(
+                        HuggingFaceRoot,
+                        "/resolve/6a1d697dd18f8c4ff031f26e6dc523ada419517d/UltraMix/4x-UltraMix_Smooth.pth"
+                    ),
+                    "7deeeac95ce7c28d616933b789f51642d169b200a6638edfb1c57ccecd903cd0"
+                )
+                {
+                    InfoUrl = new Uri("https://github.com/Kim2091"),
+                    Author = "Kim2091",
+                    LicenseType = "CC BY-NC-SA 4.0",
+                    LicenseUrl = new Uri(HuggingFaceRoot, "UltraMix/LICENSE.txt"),
+                    ContextType = SharedFolderType.ESRGAN
+                }
+            },
+            new ComfyUpscaler("4x-UltraMix_Restore.pth", ComfyUpscalerType.DownloadableModel)
+            {
+                DownloadableResource = new RemoteResource(
+                    new Uri(
+                        HuggingFaceRoot,
+                        "/resolve/6a1d697dd18f8c4ff031f26e6dc523ada419517d/UltraMix/4x-UltraMix_Restore.pth"
+                    ),
+                    "e8982b435557baeea5a08066aede41a9b3c8a6512c8688dab6d326e91ba82fa3"
+                )
+                {
+                    InfoUrl = new Uri("https://github.com/Kim2091"),
+                    Author = "Kim2091",
+                    LicenseType = "CC BY-NC-SA 4.0",
+                    LicenseUrl = new Uri(HuggingFaceRoot, "UltraMix/LICENSE.txt"),
+                    ContextType = SharedFolderType.ESRGAN
+                }
+            },
+            new ComfyUpscaler("4x-UltraMix_Balanced.pth", ComfyUpscalerType.DownloadableModel)
+            {
+                DownloadableResource = new RemoteResource(
+                    new Uri(
+                        HuggingFaceRoot,
+                        "/resolve/6a1d697dd18f8c4ff031f26e6dc523ada419517d/UltraMix/4x-UltraMix_Balanced.pth"
+                    ),
+                    "e23ca000107aae95ec9b8d7d1bf150f7884f1361cd9d669bdf824d72529f0e26"
+                )
+                {
+                    InfoUrl = new Uri("https://github.com/Kim2091"),
+                    Author = "Kim2091",
+                    LicenseType = "CC BY-NC-SA 4.0",
+                    LicenseUrl = new Uri(HuggingFaceRoot, "UltraMix/LICENSE.txt"),
+                    ContextType = SharedFolderType.ESRGAN
+                }
             }
         };
 

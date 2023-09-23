d7f8b559cafd55f52d9e3d3de0a59f02d37c9629
Ionite <dev@ionite.io>
2023-09-23 12:50:56 -0400

    Add Paginator control

---
+++ StabilityMatrix.Avalonia/Converters/StringFormatConverters.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Data.Converters;
+
+namespace StabilityMatrix.Avalonia.Converters;
+
+public static class StringFormatConverters
+{
+    private static StringFormatValueConverter? _decimalConverter;
+    public static StringFormatValueConverter Decimal =>
+        _decimalConverter ??= new StringFormatValueConverter("{0:D}", null);
+}

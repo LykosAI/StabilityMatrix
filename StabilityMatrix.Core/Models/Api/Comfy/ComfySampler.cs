598371e14086e136d7959362fa8087bb1f36cbfb
Ionite <dev@ionite.io>
2023-09-28 16:19:48 -0400

    Add Sampler Scheduler conversions

--- StabilityMatrix.Core/Models/Api/Comfy/ComfySampler.cs
+++ StabilityMatrix.Core/Models/Api/Comfy/ComfySampler.cs
@@ -1,39 +1,74 @@
 ﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 
 namespace StabilityMatrix.Core.Models.Api.Comfy;
 
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
 public readonly record struct ComfySampler(string Name)
 {
-    private static Dictionary<string, string> ConvertDict { get; } =
+    public static ComfySampler Euler { get; } = new("euler");
+    public static ComfySampler EulerAncestral { get; } = new("euler_ancestral");
+    public static ComfySampler Heun { get; } = new("heun");
+    public static ComfySampler Dpm2 { get; } = new("dpm_2");
+    public static ComfySampler Dpm2Ancestral { get; } = new("dpm_2_ancestral");
+    public static ComfySampler LMS { get; } = new("lms");
+    public static ComfySampler DpmFast { get; } = new("dpm_fast");
+    public static ComfySampler DpmAdaptive { get; } = new("dpm_adaptive");
+    public static ComfySampler Dpmpp2SAncestral { get; } = new("dpmpp_2s_ancestral");
+    public static ComfySampler DpmppSde { get; } = new("dpmpp_sde");
+    public static ComfySampler DpmppSdeGpu { get; } = new("dpmpp_sde_gpu");
+    public static ComfySampler Dpmpp2M { get; } = new("dpmpp_2m");
+    public static ComfySampler Dpmpp2MSde { get; } = new("dpmpp_2m_sde");
+    public static ComfySampler Dpmpp2MSdeGpu { get; } = new("dpmpp_2m_sde_gpu");
+    public static ComfySampler Dpmpp3M { get; } = new("dpmpp_3m");
+    public static ComfySampler Dpmpp3MSde { get; } = new("dpmpp_3m_sde");
+    public static ComfySampler Dpmpp3MSdeGpu { get; } = new("dpmpp_3m_sde_gpu");
+    public static ComfySampler DDIM { get; } = new("ddim");
+    public static ComfySampler UniPC { get; } = new("uni_pc");
+    public static ComfySampler UniPCBh2 { get; } = new("uni_pc_bh2");
+
+    private static Dictionary<ComfySampler, string> ConvertDict { get; } =
         new()
         {
-            ["euler"] = "Euler",
-            ["euler_ancestral"] = "Euler Ancestral",
-            ["heun"] = "Heun",
-            ["dpm_2"] = "DPM 2",
-            ["dpm_2_ancestral"] = "DPM 2 Ancestral",
-            ["lms"] = "LMS",
-            ["dpm_fast"] = "DPM Fast",
-            ["dpm_adaptive"] = "DPM Adaptive",
-            ["dpmpp_2s_ancestral"] = "DPM++ 2S Ancestral",
-            ["dpmpp_sde"] = "DPM++ SDE",
-            ["dpmpp_sde_gpu"] = "DPM++ SDE GPU",
-            ["dpmpp_2m"] = "DPM++ 2M",
-            ["dpmpp_2m_sde"] = "DPM++ 2M SDE",
-            ["dpmpp_2m_sde_gpu"] = "DPM++ 2M SDE GPU",
-            ["dpmpp_3m"] = "DPM++ 3M",
-            ["dpmpp_3m_sde"] = "DPM++ 3M SDE",
-            ["dpmpp_3m_sde_gpu"] = "DPM++ 3M SDE GPU",
-            ["ddim"] = "DDIM",
-            ["uni_pc"] = "UniPC",
-            ["uni_pc_bh2"] = "UniPC BH2"
+            [Euler] = "Euler",
+            [EulerAncestral] = "Euler Ancestral",
+            [Heun] = "Heun",
+            [Dpm2] = "DPM 2",
+            [Dpm2Ancestral] = "DPM 2 Ancestral",
+            [LMS] = "LMS",
+            [DpmFast] = "DPM Fast",
+            [DpmAdaptive] = "DPM Adaptive",
+            [Dpmpp2SAncestral] = "DPM++ 2S Ancestral",
+            [DpmppSde] = "DPM++ SDE",
+            [DpmppSdeGpu] = "DPM++ SDE GPU",
+            [Dpmpp2M] = "DPM++ 2M",
+            [Dpmpp2MSde] = "DPM++ 2M SDE",
+            [Dpmpp2MSdeGpu] = "DPM++ 2M SDE GPU",
+            [Dpmpp3M] = "DPM++ 3M",
+            [Dpmpp3MSde] = "DPM++ 3M SDE",
+            [Dpmpp3MSdeGpu] = "DPM++ 3M SDE GPU",
+            [DDIM] = "DDIM",
+            [UniPC] = "UniPC",
+            [UniPCBh2] = "UniPC BH2"
         };
 
     public static IReadOnlyList<ComfySampler> Defaults { get; } =
-        ConvertDict.Keys.Select(k => new ComfySampler(k)).ToImmutableArray();
+        ConvertDict.Keys.ToImmutableArray();
 
     public string DisplayName =>
-        ConvertDict.TryGetValue(Name, out var displayName) ? displayName : Name;
+        ConvertDict.TryGetValue(this, out var displayName) ? displayName : Name;
+
+    /// <inheritdoc />
+    public bool Equals(ComfySampler other)
+    {
+        return Name == other.Name;
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
 
     private sealed class NameEqualityComparer : IEqualityComparer<ComfySampler>
     {
---
+++ StabilityMatrix.Core/Models/Api/Comfy/ComfySamplerScheduler.cs
@@ -0,0 +1,25 @@
+﻿namespace StabilityMatrix.Core.Models.Api.Comfy;
+
+/// <summary>
+/// Pair of <see cref="ComfySampler"/> and <see cref="ComfyScheduler"/>
+/// </summary>
+public readonly record struct ComfySamplerScheduler(ComfySampler Sampler, ComfyScheduler Scheduler)
+{
+    /// <inheritdoc />
+    public bool Equals(ComfySamplerScheduler other)
+    {
+        return Sampler.Equals(other.Sampler) && Scheduler.Equals(other.Scheduler);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Sampler, Scheduler);
+    }
+
+    // Implicit conversion from (ComfySampler, ComfyScheduler)
+    public static implicit operator ComfySamplerScheduler((ComfySampler, ComfyScheduler) tuple)
+    {
+        return new ComfySamplerScheduler(tuple.Item1, tuple.Item2);
+    }
+}

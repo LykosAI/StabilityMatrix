1fc560dd9e0a546560883103e236d638acdde152
Ionite <dev@ionite.io>
2023-10-10 20:02:24 -0400

    Add EditableModule

---
+++ StabilityMatrix.Avalonia/Models/Inference/EditableModule.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using StabilityMatrix.Avalonia.Services;
+using StabilityMatrix.Avalonia.ViewModels.Base;
+using StabilityMatrix.Avalonia.ViewModels.Inference;
+using StabilityMatrix.Core.Models.Base;
+
+namespace StabilityMatrix.Avalonia.Models.Inference;
+
+public record EditableModule : StringValue
+{
+    public static readonly EditableModule FreeU =
+        new(
+            "FreeU",
+            builder =>
+                builder.Get<StackExpanderViewModel>(vm =>
+                {
+                    vm.Title = "FreeU";
+                    vm.AddCards(builder.Get<FreeUCardViewModel>());
+                })
+        );
+
+    public static readonly EditableModule HiresFix =
+        new(
+            "HiresFix",
+            builder =>
+                builder.Get<StackExpanderViewModel>(vm =>
+                {
+                    vm.Title = "HiresFix";
+                    vm.AddCards(
+                        builder.Get<UpscalerCardViewModel>(),
+                        builder.Get<SamplerCardViewModel>(vmSampler =>
+                        {
+                            vmSampler.IsDenoiseStrengthEnabled = true;
+                        })
+                    );
+                })
+        );
+
+    public static readonly EditableModule Upscaler =
+        new(
+            "Upscaler",
+            builder =>
+                builder.Get<StackExpanderViewModel>(vm =>
+                {
+                    vm.Title = "Upscaler";
+                    vm.AddCards(builder.Get<UpscalerCardViewModel>());
+                })
+        );
+
+    public Func<ServiceManager<ViewModelBase>, ViewModelBase> Build { get; }
+
+    private EditableModule(string value, Func<ServiceManager<ViewModelBase>, ViewModelBase> build)
+        : base(value)
+    {
+        Build = build;
+    }
+}

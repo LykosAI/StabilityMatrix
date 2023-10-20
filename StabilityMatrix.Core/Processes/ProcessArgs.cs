56935e50ea5f3237d8e7de42c2f22e7c48104822
Ionite <dev@ionite.io>
2023-10-20 18:44:45 -0400

    Add ProcessArgs and tests

---
+++ StabilityMatrix.Core/Processes/ProcessArgs.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+using OneOf;
+
+namespace StabilityMatrix.Core.Processes;
+
+public partial class ProcessArgs : OneOfBase<string, string[]>
+{
+    /// <inheritdoc />
+    private ProcessArgs(OneOf<string, string[]> input)
+        : base(input) { }
+
+    // Implicit conversions
+
+    public static implicit operator ProcessArgs(string input) => new(input);
+
+    public static implicit operator ProcessArgs(string[] input) => new(input);
+
+    public static implicit operator string(ProcessArgs input) =>
+        input.Match(str => str, arr => string.Join(' ', arr.Select(ProcessRunner.Quote)));
+
+    public static implicit operator string[](ProcessArgs input) =>
+        input.Match(
+            str => ArgumentsRegex().Matches(str).Select(x => x.Value.Trim('"')).ToArray(),
+            arr => arr
+        );
+
+    [GeneratedRegex("""[\"].+?[\"]|[^ ]+""", RegexOptions.IgnoreCase)]
+    private static partial Regex ArgumentsRegex();
+}

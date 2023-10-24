75c48909faa0f3a534f95f095ed50a3c35884c5f
Ionite <dev@ionite.io>
2023-10-24 18:47:51 -0400

    Add Conditioning properties to ModuleApplyStepEventArgs

--- StabilityMatrix.Avalonia/Models/Inference/ModuleApplyStepEventArgs.cs
+++ StabilityMatrix.Avalonia/Models/Inference/ModuleApplyStepEventArgs.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
+using StabilityMatrix.Core.Models.Api.Comfy.NodeTypes;
 
 namespace StabilityMatrix.Avalonia.Models.Inference;
 
 public class ModuleApplyStepEventArgs : EventArgs
 {
     public required ComfyNodeBuilder Builder { get; init; }
+
+    public NodeDictionary Nodes => Builder.Nodes;
+
+    /// <summary>
+    /// Temporary conditioning apply step, used by samplers to apply control net.
+    /// </summary>
+    public (
+        ConditioningNodeConnection Positive,
+        ConditioningNodeConnection Negative
+    ) Conditioning { get; set; }
 
     /// <summary>
     /// Index of the step in the pipeline.
@@ -18,6 +29,9 @@ public class ModuleApplyStepEventArgs : EventArgs
     /// </summary>
     public int StepTypeIndex { get; init; }
 
+    /// <summary>
+    /// Generation overrides (like hires fix generate, current seed generate, etc.)
+    /// </summary>
     public IReadOnlyDictionary<Type, bool> IsEnabledOverrides { get; init; } =
         new Dictionary<Type, bool>();
 }

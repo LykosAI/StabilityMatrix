3681e404270e3d08ecbc843ceb9afa8908106318
Ionite <dev@ionite.io>
2023-10-25 19:44:45 -0400

    Add Uri.Append extension

---
+++ StabilityMatrix.Core/Extensions/UriExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace StabilityMatrix.Core.Extensions;
+
+public static class UriExtensions
+{
+    /// <summary>
+    /// Return a new <see cref="Uri"/> with the given paths appended to the original.
+    /// </summary>
+    public static Uri Append(this Uri uri, params string[] paths)
+    {
+        return new Uri(
+            paths.Aggregate(
+                uri.AbsoluteUri,
+                (current, path) => $"{current.TrimEnd('/')}/{path.TrimStart('/')}"
+            )
+        );
+    }
+}

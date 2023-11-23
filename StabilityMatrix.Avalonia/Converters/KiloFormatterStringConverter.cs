b1259b4a9ba74aa701efc863760a4d971ccc2b2b
Ionite <dev@ionite.io>
2023-11-22 23:10:22 -0500

    Add Kilo and Mega prefix formatters for model browser

---
+++ StabilityMatrix.Avalonia/Converters/KiloFormatterStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace StabilityMatrix.Avalonia.Converters;
+
+public class KiloFormatterStringConverter : IValueConverter
+{
+    /// <inheritdoc />
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is null ? null : string.Format(new KiloFormatter(), "{0:K}", value);
+    }
+
+    /// <inheritdoc />
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    )
+    {
+        return value is null ? null : throw new NotImplementedException();
+    }
+}

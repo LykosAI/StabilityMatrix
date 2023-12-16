097845a70984eb7ec5eb9f15da8ffa006461a603
Ionite <dev@ionite.io>
2023-12-16 16:14:12 -0500

    Add NullableExtensions.Unwrap

---
+++ StabilityMatrix.Core/Extensions/NullableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace StabilityMatrix.Core.Extensions;
+
+public static class NullableExtensions
+{
+    /// <summary>
+    /// Unwraps a nullable object, throwing an exception if it is null.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if (<typeparamref name="T"/>) <paramref name="obj"/> is null.
+    /// </exception>
+    [DebuggerStepThrough]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T Unwrap<T>([NotNull] this T? obj, [CallerArgumentExpression("obj")] string? paramName = null)
+        where T : class
+    {
+        if (obj is null)
+        {
+            throw new ArgumentNullException(paramName, $"Unwrap of a null value ({typeof(T)}) {paramName}.");
+        }
+        return obj;
+    }
+}

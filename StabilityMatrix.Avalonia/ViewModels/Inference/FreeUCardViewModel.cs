cc9dfbb33f2f67fbdcd6f94aaa9063c6daa7e097
JT <mohnjiles@gmail.com>
2023-12-15 23:44:08 -0800

    also add some NotifyDataErrorInfo

--- StabilityMatrix.Avalonia/ViewModels/Inference/FreeUCardViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Inference/FreeUCardViewModel.cs
@@ -14,21 +14,25 @@ public partial class FreeUCardViewModel : LoadableViewModelBase
     public const string ModuleKey = "FreeU";
 
     [ObservableProperty]
+    [NotifyDataErrorInfo]
     [Required]
     [Range(0D, 10D)]
     private double b1 = 1.5;
 
     [ObservableProperty]
+    [NotifyDataErrorInfo]
     [Required]
     [Range(0D, 10D)]
     private double b2 = 1.6;
 
     [ObservableProperty]
+    [NotifyDataErrorInfo]
     [Required]
     [Range(0D, 10D)]
     private double s1 = 0.9;
 
     [ObservableProperty]
+    [NotifyDataErrorInfo]
     [Required]
     [Range(0D, 10D)]
     private double s2 = 0.2;

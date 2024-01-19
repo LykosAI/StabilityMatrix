47736c828cd2ba222d12292f4346b2a11871df50
Ionite <dev@ionite.io>
2024-01-19 15:28:15 -0500

    Add copy button for exception dialog

--- StabilityMatrix.Avalonia/ViewModels/Dialogs/ExceptionViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Dialogs/ExceptionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Sentry;
 using StabilityMatrix.Avalonia.ViewModels.Base;
 using StabilityMatrix.Avalonia.Views.Dialogs;
 using StabilityMatrix.Core.Attributes;
@@ -12,7 +13,41 @@ public partial class ExceptionViewModel : ViewModelBase
 {
     public Exception? Exception { get; set; }
 
+    public SentryId? SentryId { get; set; }
+
     public string? Message => Exception?.Message;
 
     public string? ExceptionType => Exception?.GetType().Name ?? "";
+
+    public string? FormatAsMarkdown()
+    {
+        if (Exception is null)
+        {
+            return null;
+        }
+
+        var message = $"## Exception\n{ExceptionType}: {Message}\n";
+
+        if (SentryId is not null)
+        {
+            message += $"### Sentry ID\n```\n{SentryId}\n```\n";
+        }
+
+        if (Exception.StackTrace != null)
+        {
+            message += $"### Stack Trace\n```\n{Exception.StackTrace}\n```\n";
+        }
+
+        if (Exception.InnerException is { } innerException)
+        {
+            message += $"## Inner Exception\n{innerException.GetType().Name}: {innerException.Message}\n";
+
+            if (innerException.StackTrace != null)
+            {
+                message += $"### Stack Trace\n```\n{innerException.StackTrace}\n```\n";
+            }
+        }
+
+        return message;
+    }
 }

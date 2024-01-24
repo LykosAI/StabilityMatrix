a35c1f37a9e8477220544dcded36c650030d0291
Ionite <dev@ionite.io>
2024-01-24 15:42:11 -0500

    Add StringValue GetValues helper

--- StabilityMatrix.Core/Models/StringValue.cs
+++ StabilityMatrix.Core/Models/StringValue.cs
@@ -1,4 +1,8 @@
-﻿namespace StabilityMatrix.Core.Models;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace StabilityMatrix.Core.Models;
 
 public abstract record StringValue(string Value) : IFormattable
 {
@@ -12,5 +16,31 @@ public abstract record StringValue(string Value) : IFormattable
     public string ToString(string? format, IFormatProvider? formatProvider)
     {
         return Value;
+    }
+
+    /// <summary>
+    /// Get all values of type <typeparamref name="T"/> as a dictionary.
+    /// Includes all public static properties.
+    /// </summary>
+    protected static Dictionary<string, T> GetValues<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)] T
+    >()
+        where T : StringValue
+    {
+        var values = new Dictionary<string, T>();
+
+        foreach (var field in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Static))
+        {
+            if (field.GetValue(null) is T value)
+            {
+                // Exclude if IgnoreDataMember
+                if (field.GetCustomAttribute<IgnoreDataMemberAttribute>() is not null)
+                    continue;
+
+                values.Add(value.Value, value);
+            }
+        }
+
+        return values;
     }
 }

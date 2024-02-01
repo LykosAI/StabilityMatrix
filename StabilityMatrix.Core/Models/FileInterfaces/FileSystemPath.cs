ccdf6c9d322cb1937eb07bc6044feede8800353f
Ionite <dev@ionite.io>
2024-02-01 14:46:20 -0500

    Equality improvements for FileSystemPath

--- StabilityMatrix.Core/Models/FileInterfaces/FileSystemPath.cs
+++ StabilityMatrix.Core/Models/FileInterfaces/FileSystemPath.cs
@@ -1,5 +1,9 @@
-﻿namespace StabilityMatrix.Core.Models.FileInterfaces;
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
 
+namespace StabilityMatrix.Core.Models.FileInterfaces;
+
+[PublicAPI]
 public class FileSystemPath : IEquatable<FileSystemPath>, IEquatable<string>, IFormattable
 {
     public string FullPath { get; }
@@ -15,6 +19,7 @@ public class FileSystemPath : IEquatable<FileSystemPath>, IEquatable<string>, IF
     protected FileSystemPath(params string[] paths)
         : this(Path.Combine(paths)) { }
 
+    /// <inheritdoc />
     public override string ToString()
     {
         return FullPath;
@@ -35,33 +40,88 @@ public class FileSystemPath : IEquatable<FileSystemPath>, IEquatable<string>, IF
         return FullPath;
     }
 
+    public static bool operator ==(FileSystemPath? left, FileSystemPath? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(FileSystemPath? left, FileSystemPath? right)
+    {
+        return !Equals(left, right);
+    }
+
+    /// <inheritdoc />
     public bool Equals(FileSystemPath? other)
     {
         if (ReferenceEquals(null, other))
             return false;
         if (ReferenceEquals(this, other))
             return true;
-        return FullPath == other.FullPath;
+
+        return string.Equals(
+            GetNormalizedPath(FullPath),
+            GetNormalizedPath(other.FullPath),
+            OperatingSystem.IsWindows() ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal
+        );
     }
 
+    /// <inheritdoc />
     public bool Equals(string? other)
     {
-        return string.Equals(FullPath, other);
+        if (ReferenceEquals(null, other))
+            return false;
+
+        return Equals((FileSystemPath)other);
     }
 
+    /// <inheritdoc />
     public override bool Equals(object? obj)
     {
-        return obj switch
-        {
-            FileSystemPath path => Equals(path),
-            string path => Equals(path),
-            _ => false
-        };
+        if (ReferenceEquals(null, obj))
+            return false;
+        if (ReferenceEquals(this, obj))
+            return true;
+        if (GetType() != obj.GetType())
+            return false;
+        return Equals((FileSystemPath)obj);
     }
 
+    /// <summary>
+    /// Normalize a path to a consistent format for comparison.
+    /// </summary>
+    /// <param name="path">Path to normalize.</param>
+    /// <returns>Normalized path.</returns>
+    [return: NotNullIfNotNull(nameof(path))]
+    private static string? GetNormalizedPath(string? path)
+    {
+        // Return null or empty paths as-is
+        if (string.IsNullOrEmpty(path))
+        {
+            return path;
+        }
+
+        if (Uri.TryCreate(path, UriKind.RelativeOrAbsolute, out var uri))
+        {
+            if (uri.IsAbsoluteUri)
+            {
+                path = uri.LocalPath;
+            }
+        }
+
+        // Get full path if possible, ignore errors like invalid chars or too long
+        try
+        {
+            path = Path.GetFullPath(path);
+        }
+        catch (SystemException) { }
+
+        return path.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+    }
+
+    /// <inheritdoc />
     public override int GetHashCode()
     {
-        return FullPath.GetHashCode();
+        return HashCode.Combine(GetType().GetHashCode(), FullPath.GetHashCode());
     }
 
     // Implicit conversions to and from string

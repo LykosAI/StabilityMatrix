5db1e6e42809bc974c2d605de37ee982528560f6
Ionite <dev@ionite.io>
2024-02-01 21:01:19 -0500

    Fix tests on linux

--- StabilityMatrix.Tests/Core/FileSystemPathTests.cs
+++ StabilityMatrix.Tests/Core/FileSystemPathTests.cs
@@ -1,13 +1,36 @@
-﻿using StabilityMatrix.Core.Models.FileInterfaces;
+﻿using System.Runtime.Versioning;
+using StabilityMatrix.Core.Models.FileInterfaces;
 
 namespace StabilityMatrix.Tests.Core;
 
 [TestClass]
 public class FileSystemPathTests
 {
+    [SupportedOSPlatform("windows")]
     [DataTestMethod]
     [DataRow("M:\\Path", "M:\\Path")]
+    [DataRow("root/abc", "root/abc")]
     [DataRow("root\\abc", "root\\abc")]
+    public void TestFilePathEqualsWin(string left, string right)
+    {
+        // Arrange
+        var leftPath = new FilePath(left);
+        var rightPath = new FilePath(right);
+
+        // Act
+        var resultEquals = leftPath.Equals(rightPath);
+        var resultOperator = leftPath == rightPath;
+        var resultNotOperator = leftPath != rightPath;
+
+        // Assert
+        Assert.IsTrue(resultEquals);
+        Assert.IsTrue(resultOperator);
+        Assert.IsFalse(resultNotOperator);
+    }
+
+    [DataTestMethod]
+    [DataRow("M:/Path", "M:/Path")]
+    [DataRow("root/abc", "root/abc")]
     [DataRow("root/abc", "root/abc")]
     public void TestFilePathEquals(string left, string right)
     {
@@ -27,8 +50,8 @@ public class FileSystemPathTests
     }
 
     [DataTestMethod]
-    [DataRow("M:\\Path", "M:\\Path2")]
-    [DataRow("root\\abc", "root\\abc2")]
+    [DataRow("M:/Path", "M:/Path2")]
+    [DataRow("root/abc", "root/abc2")]
     public void TestFilePathNotEquals(string left, string right)
     {
         // Arrange
@@ -47,8 +70,8 @@ public class FileSystemPathTests
     }
 
     [DataTestMethod]
-    [DataRow("M:\\Path", "M:\\Path")]
-    [DataRow("M:\\Path", "M:\\Path\\")]
+    [DataRow("M:/Path", "M:/Path")]
+    [DataRow("M:/Path", "M:/Path/")]
     [DataRow("root/abc", "root/abc")]
     [DataRow("root/abc", "root/abc/")]
     public void TestDirectoryPathEquals(string left, string right)

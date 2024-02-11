2998ceee2122c17630e633fc611dec9df8fec6f2
JT <mohnjiles@gmail.com>
2024-02-10 22:35:50 -0800

    Added models & refit interface for openart stuff

---
+++ StabilityMatrix.Core/Models/Api/OpenArt/OpenArtFeedRequest.cs
@@ -0,0 +1,18 @@
+﻿using Refit;
+
+namespace StabilityMatrix.Core.Models.Api.OpenArt;
+
+public class OpenArtFeedRequest
+{
+    [AliasAs("category")]
+    public string Category { get; set; }
+
+    [AliasAs("sort")]
+    public string Sort { get; set; }
+
+    [AliasAs("custom_node")]
+    public string CustomNode { get; set; }
+
+    [AliasAs("cursor")]
+    public string Cursor { get; set; }
+}
---
+++ StabilityMatrix.Core/Models/Api/OpenArt/OpenArtSearchResult.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StabilityMatrix.Core.Models.Api.OpenArt;
+
+public class OpenArtSearchResult
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("creator")]
+    public OpenArtCreator Creator { get; set; }
+
+    [JsonPropertyName("updated_at")]
+    public DateTimeOffset UpdatedAt { get; set; }
+
+    [JsonPropertyName("stats")]
+    public OpenArtStats Stats { get; set; }
+
+    [JsonPropertyName("nodes_index")]
+    public IEnumerable<string> NodesIndex { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTimeOffset CreatedAt { get; set; }
+
+    [JsonPropertyName("categories")]
+    public IEnumerable<string> Categories { get; set; }
+
+    [JsonPropertyName("thumbnails")]
+    public IEnumerable<OpenArtThumbnail> Thumbnails { get; set; }
+
+    [JsonPropertyName("nodes_count")]
+    public NodesCount NodesCount { get; set; }
+}

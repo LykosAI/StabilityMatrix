31f059f12e18a592faa352d6153e9400a9065af1
Ionite <dev@ionite.io>
2024-02-18 19:01:24 -0500

    Remove NodeConnectionBase implicit array conversion
    Wasn't used anyways

--- StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/NodeConnectionBase.cs
+++ StabilityMatrix.Core/Models/Api/Comfy/NodeTypes/NodeConnectionBase.cs
@@ -3,10 +3,4 @@
 public abstract class NodeConnectionBase
 {
     public object[]? Data { get; set; }
-
-    // Implicit conversion to object[]
-    public static implicit operator object[](NodeConnectionBase nodeConnection)
-    {
-        return nodeConnection.Data ?? Array.Empty<object>();
-    }
 }

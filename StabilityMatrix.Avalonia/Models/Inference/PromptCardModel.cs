74605f6513bb6a1f3e8bafbfb30b129aa8a02d8e
Ionite <dev@ionite.io>
2024-02-21 16:35:08 -0500

    oops

--- StabilityMatrix.Avalonia/Models/Inference/PromptCardModel.cs
+++ StabilityMatrix.Avalonia/Models/Inference/PromptCardModel.cs
@@ -1,11 +1,10 @@
 ﻿using System.Text.Json.Nodes;
 
 namespace StabilityMatrix.Avalonia.Models.Inference;
-]
+
 public class PromptCardModel
 {
     public string? Prompt { get; init; }
     public string? NegativePrompt { get; init; }
-    
     public JsonObject? ModulesCardState { get; init; }
 }

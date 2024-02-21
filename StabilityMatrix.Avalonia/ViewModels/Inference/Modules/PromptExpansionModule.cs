2d22db1374da7c84d6c63babe234f0a9539ae262
Ionite <dev@ionite.io>
2024-02-21 02:19:47 -0500

    Implement Prompt Expansion module step

--- StabilityMatrix.Avalonia/ViewModels/Inference/Modules/PromptExpansionModule.cs
+++ StabilityMatrix.Avalonia/ViewModels/Inference/Modules/PromptExpansionModule.cs
@@ -3,6 +3,7 @@ using StabilityMatrix.Avalonia.Models.Inference;
 using StabilityMatrix.Avalonia.Services;
 using StabilityMatrix.Avalonia.ViewModels.Base;
 using StabilityMatrix.Core.Attributes;
+using StabilityMatrix.Core.Models.Api.Comfy.Nodes;
 
 namespace StabilityMatrix.Avalonia.ViewModels.Inference.Modules;
 
@@ -19,6 +20,30 @@ public class PromptExpansionModule : ModuleBase
 
     protected override void OnApplyStep(ModuleApplyStepEventArgs e)
     {
-        throw new NotImplementedException();
+        var promptExpansionCard = GetCard<PromptExpansionCardViewModel>();
+
+        var model =
+            promptExpansionCard.SelectedModel
+            ?? throw new InvalidOperationException($"{Title}: Model not selected");
+
+        e.Builder.Connections.PositivePrompt = e.Nodes.AddTypedNode(
+            new ComfyNodeBuilder.PromptExpansion
+            {
+                Name = e.Nodes.GetUniqueName("PromptExpansion_Positive"),
+                ModelName = model.RelativePath,
+                Text = e.Builder.Connections.PositivePrompt,
+                Seed = e.Builder.Connections.Seed
+            }
+        ).Output;
+
+        e.Builder.Connections.NegativePrompt = e.Nodes.AddTypedNode(
+            new ComfyNodeBuilder.PromptExpansion
+            {
+                Name = e.Nodes.GetUniqueName("PromptExpansion_Negative"),
+                ModelName = model.RelativePath,
+                Text = e.Builder.Connections.NegativePrompt,
+                Seed = e.Builder.Connections.Seed
+            }
+        ).Output;
     }
 }

6d0d331937e7939de021af83d08dc56acdceefad
Ionite <dev@ionite.io>
2024-03-13 19:22:58 -0400

    Add PenPath models

---
+++ StabilityMatrix.Avalonia/Controls/Models/PenPath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using SkiaSharp;
+
+namespace StabilityMatrix.Avalonia.Controls.Models;
+
+public readonly record struct PenPath
+{
+    public PenPath(SKPath path)
+    {
+        Path = path;
+    }
+
+    public SKPath Path { get; init; }
+
+    public SKColor FillColor { get; init; }
+
+    public List<PenPoint> Points { get; init; } = [];
+}
---
+++ StabilityMatrix.Avalonia/Controls/Models/PenPoint.cs
@@ -0,0 +1,19 @@
+﻿using SkiaSharp;
+
+namespace StabilityMatrix.Avalonia.Controls.Models;
+
+public readonly record struct PenPoint
+{
+    public PenPoint(SKPoint point, double radius = 1, double? pressure = null)
+    {
+        Point = point;
+        Radius = radius;
+        Pressure = pressure;
+    }
+
+    public SKPoint Point { get; init; }
+
+    public double Radius { get; init; }
+
+    public double? Pressure { get; init; }
+}

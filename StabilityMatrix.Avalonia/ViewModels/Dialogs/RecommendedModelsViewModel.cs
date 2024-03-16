38013c16d6665f3401c891de8225306a84942446
JT <mohnjiles@gmail.com>
2024-03-15 18:04:55 -0700

    Fix star ratings for civitai stuff

--- StabilityMatrix.Avalonia/ViewModels/Dialogs/RecommendedModelsViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Dialogs/RecommendedModelsViewModel.cs
@@ -97,9 +97,11 @@ public partial class RecommendedModelsViewModel : ContentDialogViewModelBase
                         new RecommendedModelItemViewModel
                         {
                             ModelVersion = model.ModelVersions.First(
-                                x => !x.BaseModel.Contains("Turbo", StringComparison.OrdinalIgnoreCase)
+                                x =>
+                                    !x.BaseModel.Contains("Turbo", StringComparison.OrdinalIgnoreCase)
+                                    && !x.BaseModel.Contains("Lightning", StringComparison.OrdinalIgnoreCase)
                             ),
-                            Author = $"by {model.Creator.Username}",
+                            Author = $"by {model.Creator?.Username}",
                             CivitModel = model
                         }
                 )

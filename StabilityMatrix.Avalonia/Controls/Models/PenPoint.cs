5c52ac401c04a40bb620783b76fad8c164579132
Ionite <dev@ionite.io>
2024-03-19 00:54:50 -0400

    Add IsPen for PenPoint, move to primary ctor

--- StabilityMatrix.Avalonia/Controls/Models/PenPoint.cs
+++ StabilityMatrix.Avalonia/Controls/Models/PenPoint.cs
@@ -2,18 +2,20 @@
 
 namespace StabilityMatrix.Avalonia.Controls.Models;
 
-public readonly record struct PenPoint
+public readonly record struct PenPoint(SKPoint Point)
 {
-    public PenPoint(SKPoint point, double radius = 1, double? pressure = null)
-    {
-        Point = point;
-        Radius = radius;
-        Pressure = pressure;
-    }
+    /// <summary>
+    /// Radius of the point.
+    /// </summary>
+    public double Radius { get; init; } = 1;
 
-    public SKPoint Point { get; init; }
-
-    public double Radius { get; init; }
-
+    /// <summary>
+    /// Optional pressure of the point. If null, the pressure is unknown.
+    /// </summary>
     public double? Pressure { get; init; }
+
+    /// <summary>
+    /// True if the point was created by a pen, false if it was created by a mouse.
+    /// </summary>
+    public bool IsPen { get; init; }
 }

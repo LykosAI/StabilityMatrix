6ab909a74e7ac41dcd7fe555f458f6b8248b4dd7
Ionite <dev@ionite.io>
2024-03-19 00:55:31 -0400

    Refactored PaintCanvas to use ViewModel

--- StabilityMatrix.Avalonia/Controls/Painting/PaintCanvas.axaml.cs
+++ StabilityMatrix.Avalonia/Controls/Painting/PaintCanvas.axaml.cs
@@ -1,17 +1,23 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.PanAndZoom;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
-using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using Avalonia.Skia;
 using Avalonia.Threading;
+using CommunityToolkit.Mvvm.Input;
+using DynamicData.Binding;
 using SkiaSharp;
 using StabilityMatrix.Avalonia.Controls.Models;
+using StabilityMatrix.Avalonia.ViewModels.Controls;
 
 namespace StabilityMatrix.Avalonia.Controls;
 
@@ -20,52 +26,19 @@ public class PaintCanvas : TemplatedControl
     private readonly ConcurrentDictionary<long, PenPath> temporaryPaths = new();
     private ImmutableList<PenPath> paths = [];
 
+    private IDisposable? viewModelSubscription;
+
     private bool isPenDown;
-    private SKColor currentBrushColor = Colors.White.ToSKColor();
+
+    private PaintCanvasViewModel? ViewModel { get; set; }
+
+    public SKBitmap? BackgroundBitmap { get; set; }
 
     private SkiaCustomCanvas? MainCanvas { get; set; }
 
-    public static readonly StyledProperty<Color?> PaintBrushColorProperty = AvaloniaProperty.Register<
-        PaintCanvas,
-        Color?
-    >(nameof(PaintBrushColor), Colors.White);
-
-    public Color? PaintBrushColor
-    {
-        get => GetValue(PaintBrushColorProperty);
-        set => SetValue(PaintBrushColorProperty, value);
-    }
-
-    public static readonly StyledProperty<float> CurrentPenPressureProperty = AvaloniaProperty.Register<
-        PaintCanvas,
-        float
-    >("CurrentPenPressure");
-
-    public float CurrentPenPressure
-    {
-        get => GetValue(CurrentPenPressureProperty);
-        set => SetValue(CurrentPenPressureProperty, value);
-    }
-
     static PaintCanvas()
     {
-        AffectsRender<ImageMaskEditor>(BoundsProperty);
-    }
-
-    public PaintCanvas()
-    {
-        // AddHandler(PointerMovedEvent, OnPointerMoved, RoutingStrategies.Tunnel);
-    }
-
-    /// <inheritdoc />
-    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
-    {
-        base.OnPropertyChanged(change);
-
-        if (change.Property == PaintBrushColorProperty)
-        {
-            currentBrushColor = (PaintBrushColor ?? Colors.Transparent).ToSKColor();
-        }
+        AffectsRender<PaintCanvas>(BoundsProperty);
     }
 
     /// <inheritdoc />
@@ -79,14 +52,70 @@ public class PaintCanvas : TemplatedControl
 
         if (MainCanvas is not null)
         {
+            // If we already have a BackgroundBitmap, scale MainCanvas to match
+            if (BackgroundBitmap is not null)
+            {
+                MainCanvas.Width = BackgroundBitmap.Width;
+                MainCanvas.Height = BackgroundBitmap.Height;
+            }
+
             MainCanvas.RenderSkia += OnRenderSkia;
+            MainCanvas.PointerEntered += MainCanvas_OnPointerEntered;
+            MainCanvas.PointerExited += MainCanvas_OnPointerExited;
         }
+
+        var zoomBorder = e.NameScope.Find<ZoomBorder>("PART_ZoomBorder");
+        if (zoomBorder is not null)
+        {
+            zoomBorder.ZoomChanged += (_, zoomEventArgs) =>
+            {
+                if (ViewModel is not null)
+                {
+                    ViewModel.CurrentZoom = zoomEventArgs.ZoomX;
+
+                    UpdateCanvasCursor();
+                }
+            };
+        }
+
+        OnDataContextChanged(EventArgs.Empty);
+    }
+
+    /// <inheritdoc />
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+
+        if (DataContext is PaintCanvasViewModel viewModel)
+        {
+            // Set the save action
+            viewModel.SaveCanvasAsImage = SaveCanvasToBitmap;
+
+            viewModelSubscription?.Dispose();
+            viewModelSubscription = viewModel
+                .WhenPropertyChanged(vm => vm.BackgroundImage)
+                .Subscribe(change =>
+                {
+                    if (MainCanvas is not null && change.Value is not null)
+                    {
+                        MainCanvas.Width = change.Value.Width;
+                        MainCanvas.Height = change.Value.Height;
+                        MainCanvas.InvalidateVisual();
+                    }
+                });
+
+            ViewModel = viewModel;
+        }
+    }
+
+    /// <inheritdoc />
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
     }
 
     private void HandlePointerEvent(PointerEventArgs e)
     {
-        var lastPointer = e.GetCurrentPoint(this);
-
         if (e.RoutedEvent == PointerReleasedEvent && e.Pointer.Type == PointerType.Touch)
         {
             temporaryPaths.TryRemove(e.Pointer.Id, out _);
@@ -94,86 +123,97 @@ public class PaintCanvas : TemplatedControl
         }
 
         // if (e.Pointer.Type != PointerType.Pen || lastPointer.Properties.Pressure > 0)
-        if (true)
+
+        e.Handled = true;
+
+        // Must have this or stylus inputs lost after a while
+        // https://github.com/AvaloniaUI/Avalonia/issues/12289#issuecomment-1695620412
+
+        e.PreventGestureRecognition();
+
+        if (DataContext is not PaintCanvasViewModel viewModel)
         {
-            e.Handled = true;
+            return;
+        }
 
-            // Must have this or stylus inputs lost after a while
-            // https://github.com/AvaloniaUI/Avalonia/issues/12289#issuecomment-1695620412
+        var currentPoint = e.GetCurrentPoint(this);
 
-            e.PreventGestureRecognition();
-
-            var currentPoint = e.GetCurrentPoint(this);
-
-            if (e.RoutedEvent == PointerPressedEvent)
+        if (e.RoutedEvent == PointerPressedEvent)
+        {
+            // Ignore if mouse and not left button
+            if (e.Pointer.Type == PointerType.Mouse && !currentPoint.Properties.IsLeftButtonPressed)
             {
-                // Ignore if mouse and not left button
-                if (e.Pointer.Type == PointerType.Mouse && !currentPoint.Properties.IsLeftButtonPressed)
-                {
-                    return;
-                }
+                return;
+            }
 
-                isPenDown = true;
+            isPenDown = true;
 
+            var cursorPosition = e.GetPosition(MainCanvas);
+
+            // Start a new path
+            var path = new SKPath();
+            path.MoveTo(cursorPosition.ToSKPoint());
+
+            temporaryPaths[e.Pointer.Id] = new PenPath(path)
+            {
+                FillColor = viewModel.PaintBrushSKColor.WithAlpha((byte)(viewModel.PaintBrushAlpha * 255))
+            };
+        }
+        else if (e.RoutedEvent == PointerReleasedEvent)
+        {
+            if (isPenDown)
+            {
+                isPenDown = false;
+            }
+
+            if (temporaryPaths.TryGetValue(e.Pointer.Id, out var path))
+            {
+                paths = paths.Add(path);
+            }
+
+            temporaryPaths.TryRemove(e.Pointer.Id, out _);
+        }
+        else
+        {
+            // Moved event
+            if (!isPenDown || currentPoint.Properties.Pressure == 0)
+            {
+                return;
+            }
+
+            // Use intermediate points to include past events we missed
+            var points = e.GetIntermediatePoints(MainCanvas);
+
+            viewModel.CurrentPenPressure = points.FirstOrDefault().Properties.Pressure;
+
+            // Get existing temp path
+            if (temporaryPaths.TryGetValue(e.Pointer.Id, out var penPath))
+            {
                 var cursorPosition = e.GetPosition(MainCanvas);
 
-                // Start a new path
-                var path = new SKPath();
-                path.MoveTo(cursorPosition.ToSKPoint());
+                // Add line for path
+                penPath.Path.LineTo(cursorPosition.ToSKPoint());
 
-                temporaryPaths[e.Pointer.Id] = new PenPath(path) { FillColor = currentBrushColor };
-            }
-            else if (e.RoutedEvent == PointerReleasedEvent)
-            {
-                if (isPenDown)
+                // Add points
+                foreach (var point in points)
                 {
-                    isPenDown = false;
-                }
+                    var skCanvasPoint = point.Position.ToSKPoint();
 
-                if (temporaryPaths.TryGetValue(e.Pointer.Id, out var path))
-                {
-                    paths = paths.Add(path);
-                }
+                    // penPath.Path.LineTo(skCanvasPoint);
 
-                temporaryPaths.TryRemove(e.Pointer.Id, out _);
-            }
-            else
-            {
-                // Moved event
-                if (!isPenDown || currentPoint.Properties.Pressure == 0)
-                {
-                    return;
-                }
-
-                // Use intermediate points to include past events we missed
-                var points = e.GetIntermediatePoints(MainCanvas);
-
-                CurrentPenPressure = points.FirstOrDefault().Properties.Pressure;
-
-                // Get existing temp path
-                if (temporaryPaths.TryGetValue(e.Pointer.Id, out var penPath))
-                {
-                    var cursorPosition = e.GetPosition(MainCanvas);
-
-                    // Add line for path
-                    penPath.Path.LineTo(cursorPosition.ToSKPoint());
-
-                    // Add points
-                    foreach (var point in points)
+                    var penPoint = new PenPoint(skCanvasPoint)
                     {
-                        var skCanvasPoint = point.Position.ToSKPoint();
+                        Pressure = point.Pointer.Type != PointerType.Mouse ? 1 : point.Properties.Pressure,
+                        Radius = viewModel.PaintBrushSize,
+                        IsPen = point.Pointer.Type == PointerType.Pen
+                    };
 
-                        // penPath.Path.LineTo(skCanvasPoint);
-
-                        var penPoint = new PenPoint(skCanvasPoint) { Pressure = point.Properties.Pressure };
-
-                        penPath.Points.Add(penPoint);
-                    }
+                    penPath.Points.Add(penPoint);
                 }
             }
-
-            Dispatcher.UIThread.Post(() => MainCanvas!.InvalidateVisual(), DispatcherPriority.Render);
         }
+
+        Dispatcher.UIThread.Post(() => MainCanvas?.InvalidateVisual(), DispatcherPriority.Render);
     }
 
     /// <inheritdoc />
@@ -195,6 +235,85 @@ public class PaintCanvas : TemplatedControl
     {
         HandlePointerEvent(e);
         base.OnPointerMoved(e);
+    }
+
+    /// <inheritdoc />
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        base.OnKeyDown(e);
+
+        if (e.Key == Key.Escape)
+        {
+            e.Handled = true;
+        }
+    }
+
+    private int lastCanvasCursorRadius;
+    private Cursor? lastCanvasCursor;
+
+    private void UpdateCanvasCursor()
+    {
+        if (MainCanvas is not { } canvas)
+        {
+            return;
+        }
+
+        var currentZoom = ViewModel?.CurrentZoom ?? 1;
+
+        // Get brush size
+        var currentBrushSize = Math.Max(ViewModel?.PaintBrushSize ?? 1, 1);
+        var brushPixels = (int)Math.Ceiling(currentBrushSize * 2 * currentZoom);
+        var brushCanvasPixels = brushPixels * 2;
+
+        // Only update cursor if brush size has changed
+        if (brushCanvasPixels == lastCanvasCursorRadius)
+        {
+            canvas.Cursor = lastCanvasCursor;
+            return;
+        }
+
+        lastCanvasCursorRadius = brushCanvasPixels;
+
+        using var cursorBitmap = new SKBitmap(brushCanvasPixels, brushCanvasPixels);
+        using var cursorCanvas = new SKCanvas(cursorBitmap);
+        cursorCanvas.Clear(SKColors.Transparent);
+        cursorCanvas.DrawCircle(
+            brushPixels,
+            brushPixels,
+            brushPixels,
+            new SKPaint
+            {
+                Color = SKColors.Black,
+                Style = SKPaintStyle.Stroke,
+                StrokeWidth = 1,
+                IsDither = true,
+                IsAntialias = true
+            }
+        );
+        cursorCanvas.Flush();
+
+        using var data = cursorBitmap.Encode(SKEncodedImageFormat.Png, 100);
+        using var stream = data.AsStream();
+
+        var bitmap = WriteableBitmap.Decode(stream);
+
+        canvas.Cursor = new Cursor(bitmap, new PixelPoint(brushCanvasPixels / 2, brushCanvasPixels / 2));
+
+        lastCanvasCursor?.Dispose();
+        lastCanvasCursor = canvas.Cursor;
+    }
+
+    private void MainCanvas_OnPointerEntered(object? sender, PointerEventArgs e)
+    {
+        UpdateCanvasCursor();
+    }
+
+    private void MainCanvas_OnPointerExited(object? sender, PointerEventArgs e)
+    {
+        if (sender is SkiaCustomCanvas canvas)
+        {
+            canvas.Cursor = new Cursor(StandardCursorType.Arrow);
+        }
     }
 
     private Point GetRelativePosition(Point pt, Visual? relativeTo)
@@ -219,13 +338,23 @@ public class PaintCanvas : TemplatedControl
         data.SaveTo(stream);
     }
 
-    private static void RenderPenPath(SKCanvas canvas, PenPath penPath)
+    public AsyncRelayCommand ClearCanvasCommand => new(ClearCanvasAsync);
+
+    public async Task ClearCanvasAsync()
     {
-        using var paint = new SKPaint();
+        paths = ImmutableList<PenPath>.Empty;
+        temporaryPaths.Clear();
+
+        await Dispatcher.UIThread.InvokeAsync(() => MainCanvas?.InvalidateVisual());
+    }
+
+    private static void RenderPenPath(SKCanvas canvas, PenPath penPath, SKPaint paint)
+    {
+        // Apply Color
         paint.Color = penPath.FillColor;
+        // Defaults
         paint.IsDither = true;
         paint.IsAntialias = true;
-        paint.StrokeWidth = 3;
         paint.Style = SKPaintStyle.Stroke;
         paint.StrokeCap = SKStrokeCap.Round;
 
@@ -235,9 +364,10 @@ public class PaintCanvas : TemplatedControl
         {
             var penPoint = penPath.Points[i];
 
-            var pressure = penPoint.Pressure ?? 0.5;
-            var thickness = pressure * 10;
-            var radius = pressure * penPoint.Radius * 1.5;
+            var radius = penPoint.Radius;
+            var pressure = penPoint.Pressure ?? 1;
+            var thickness = pressure * radius * 2;
+            // var radius = pressure * penPoint.Radius * 1.5;
 
             // Draw path
             if (i < penPath.Points.Count - 1)
@@ -249,71 +379,36 @@ public class PaintCanvas : TemplatedControl
             // Only draw circle if pressure is high enough
             if (penPoint.Pressure > 0.1)
             {
+                paint.StrokeWidth = (float)(thickness * 0.5);
                 canvas.DrawCircle(penPoint.Point, (float)radius, paint);
             }
         }
     }
 
-    public void OnRenderSkia(SKCanvas canvas)
+    private void OnRenderSkia(SKCanvas canvas)
     {
-        // canvas.Clear();
+        // Draw background color
+        canvas.Clear(SKColors.Transparent);
 
-        SKPaint? paint = null;
-
-        try
+        // Draw background image if set
+        if (ViewModel?.BackgroundImage is { } backgroundImage)
         {
-            // Draw the paths
-            foreach (var penPath in temporaryPaths.Values)
-            {
-                RenderPenPath(canvas, penPath);
-            }
-
-            foreach (var penPath in paths)
-            {
-                RenderPenPath(canvas, penPath);
-            }
-
-            /*foreach (var penPath in temporaryPaths.Values)
-            {
-                if (paint?.Color != penPath.FillColor)
-                {
-                    paint?.Dispose();
-            
-                    paint = new SKPaint();
-                    paint.Color = penPath.FillColor;
-                    paint.IsDither = true;
-                    paint.IsAntialias = true;
-                    paint.StrokeWidth = 3;
-                    paint.Style = SKPaintStyle.Stroke;
-                    paint.StrokeCap = SKStrokeCap.Round;
-                }
-            
-                canvas.DrawPath(penPath.Path, paint);
-            }
-            
-            foreach (var penPath in paths)
-            {
-                if (paint?.Color != penPath.FillColor)
-                {
-                    paint?.Dispose();
-            
-                    paint = new SKPaint();
-                    paint.Color = penPath.FillColor;
-                    paint.IsDither = true;
-                    paint.IsAntialias = true;
-                    paint.StrokeWidth = 3;
-                    paint.Style = SKPaintStyle.Stroke;
-                    paint.StrokeCap = SKStrokeCap.Round;
-                }
-            
-                canvas.DrawPath(penPath.Path, paint);
-            }*/
-
-            canvas.Flush();
+            canvas.DrawBitmap(backgroundImage, new SKPoint(0, 0));
         }
-        finally
+
+        using var paint = new SKPaint();
+
+        // Draw the paths
+        foreach (var penPath in temporaryPaths.Values)
         {
-            paint?.Dispose();
+            RenderPenPath(canvas, penPath, paint);
         }
+
+        foreach (var penPath in paths)
+        {
+            RenderPenPath(canvas, penPath, paint);
+        }
+
+        canvas.Flush();
     }
 }
---
+++ StabilityMatrix.Avalonia/ViewModels/Controls/PaintCanvasViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Avalonia.Media;
+using Avalonia.Skia;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SkiaSharp;
+
+namespace StabilityMatrix.Avalonia.ViewModels.Controls;
+
+public partial class PaintCanvasViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private Color? paintBrushColor;
+
+    public SKColor PaintBrushSKColor => (PaintBrushColor ?? Colors.Transparent).ToSKColor();
+
+    [ObservableProperty]
+    private double paintBrushSize = 12;
+
+    [ObservableProperty]
+    private double paintBrushAlpha = 1;
+
+    [ObservableProperty]
+    private double currentPenPressure;
+
+    [ObservableProperty]
+    private double currentZoom;
+
+    [ObservableProperty]
+    private bool isPenDown;
+
+    [ObservableProperty]
+    private SKBitmap? backgroundImage;
+
+    public Action<Stream>? SaveCanvasAsImage { get; set; }
+}

255b1feb66fb4bff45dd267554489ef7920fe9c9
Ionite <dev@ionite.io>
2024-04-03 20:34:32 -0400

    Add LayerDiffuseMode

---
+++ StabilityMatrix.Core/Models/Inference/LayerDiffuseMode.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StabilityMatrix.Core.Models.Inference;
+
+public enum LayerDiffuseMode
+{
+    /// <summary>
+    /// The layer diffuse mode is not set.
+    /// </summary>
+    [Display(Name = "None")]
+    None,
+
+    /// <summary>
+    /// Generate foreground only with transparency.
+    /// </summary>
+    [Display(Name = "Generate Foreground with Transparency")]
+    GenerateForegroundWithTransparency,
+}

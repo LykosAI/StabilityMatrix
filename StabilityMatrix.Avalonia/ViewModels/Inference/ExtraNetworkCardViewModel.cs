f2b96fb5864cf82df3bee089ce2adc12451bbd1a
Ionite <dev@ionite.io>
2024-04-18 20:10:03 -0400

    Add ExtraNetworkCard

---
+++ StabilityMatrix.Avalonia/ViewModels/Inference/ExtraNetworkCardViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using StabilityMatrix.Avalonia.Controls;
+using StabilityMatrix.Avalonia.Models.Inference;
+using StabilityMatrix.Avalonia.Services;
+using StabilityMatrix.Avalonia.ViewModels.Base;
+using StabilityMatrix.Core.Attributes;
+using StabilityMatrix.Core.Models;
+
+namespace StabilityMatrix.Avalonia.ViewModels.Inference;
+
+[View(typeof(ExtraNetworkCard))]
+[ManagedService]
+[Transient]
+public partial class ExtraNetworkCardViewModel(IInferenceClientManager clientManager)
+    : LoadableViewModelBase,
+        IComfyStep
+{
+    public const string ModuleKey = "ExtraNetwork";
+
+    [ObservableProperty]
+    private HybridModelFile? selectedModel;
+
+    [ObservableProperty]
+    private double modelWeight = 1.0;
+
+    [ObservableProperty]
+    private bool isClipWeightEnabled;
+
+    [ObservableProperty]
+    private double clipWeight = 1.0;
+
+    public IInferenceClientManager ClientManager { get; } = clientManager;
+}

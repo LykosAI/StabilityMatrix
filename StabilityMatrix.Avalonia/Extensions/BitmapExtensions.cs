8ed3fb26b4b928fa5c2edf690e3ee300b975b929
Ionite <dev@ionite.io>
2024-05-28 18:17:50 -0400

    Create BitmapExtensions.cs

---
+++ StabilityMatrix.Avalonia/Extensions/BitmapExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Media.Imaging;
+using SkiaSharp;
+
+namespace StabilityMatrix.Avalonia.Extensions;
+
+public static class BitmapExtensions
+{
+    /// <summary>
+    /// Converts an Avalonia <see cref="IBitmap"/> to a SkiaSharp <see cref="SKBitmap"/>.
+    /// </summary>
+    /// <param name="bitmap">The Avalonia bitmap to convert.</param>
+    /// <returns>The SkiaSharp bitmap.</returns>
+    public static SKBitmap ToSKBitmap(this Bitmap bitmap)
+    {
+        var skBitmap = new SKBitmap(
+            bitmap.PixelSize.Width,
+            bitmap.PixelSize.Height,
+            SKColorType.Bgra8888,
+            SKAlphaType.Premul
+        );
+
+        var stride = skBitmap.RowBytes;
+        var bufferSize = stride * skBitmap.Height;
+        var sourceRect = new PixelRect(0, 0, bitmap.PixelSize.Width, bitmap.PixelSize.Height);
+
+        bitmap.CopyPixels(sourceRect, skBitmap.GetPixels(), bufferSize, stride);
+
+        return skBitmap;
+    }
+}

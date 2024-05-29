333ad6de6de4989c561672757eeec7e4a7969431
Ionite <dev@ionite.io>
2024-05-29 01:34:04 -0400

    Add bitmap to byte array extensions

--- StabilityMatrix.Avalonia/Extensions/BitmapExtensions.cs
+++ StabilityMatrix.Avalonia/Extensions/BitmapExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Avalonia;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Avalonia;
 using Avalonia.Media.Imaging;
 using SkiaSharp;
 
@@ -27,5 +30,28 @@ public static class BitmapExtensions
         bitmap.CopyPixels(sourceRect, skBitmap.GetPixels(), bufferSize, stride);
 
         return skBitmap;
+    }
+
+    // Convert to byte array
+    public static byte[] ToByteArray(this Bitmap bitmap)
+    {
+        var pixelRect = new PixelRect(0, 0, bitmap.PixelSize.Width, bitmap.PixelSize.Height);
+        var stride = bitmap.PixelSize.Width * 4;
+
+        var bufferSize = bitmap.PixelSize.Width * bitmap.PixelSize.Height * 4;
+        var buffer = new byte[bufferSize];
+
+        var pinnedBuffer = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+
+        try
+        {
+            bitmap.CopyPixels(pixelRect, pinnedBuffer.AddrOfPinnedObject(), bufferSize, stride);
+        }
+        finally
+        {
+            pinnedBuffer.Free();
+        }
+
+        return buffer;
     }
 }

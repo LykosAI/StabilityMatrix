d99cc2f369a8227d2b2d437f18c202c29edfeb25
Ionite <dev@ionite.io>
2024-06-07 18:37:14 -0400

    Fixed incorrect JsonSerializable usages

--- StabilityMatrix.Avalonia/Models/Inference/StackCardModel.cs
+++ StabilityMatrix.Avalonia/Models/Inference/StackCardModel.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Nodes;
-using System.Text.Json.Serialization;
 
 namespace StabilityMatrix.Avalonia.Models.Inference;
 
-[JsonSerializable(typeof(StackCardModel))]
 public class StackCardModel
 {
     public List<JsonObject>? Cards { get; init; }
--- StabilityMatrix.Avalonia/Models/Inference/ViewState.cs
+++ StabilityMatrix.Avalonia/Models/Inference/ViewState.cs
@@ -1,11 +1,8 @@
-﻿using System.Text.Json.Serialization;
-
-namespace StabilityMatrix.Avalonia.Models.Inference;
+﻿namespace StabilityMatrix.Avalonia.Models.Inference;
 
 /// <summary>
 /// Model for view states of inference tabs
 /// </summary>
-[JsonSerializable(typeof(ViewState))]
 public class ViewState
 {
     public string? DockLayout { get; set; }

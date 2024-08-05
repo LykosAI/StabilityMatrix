309f88deadc01b9b61b455fc3224a12cce1dd4f8
JT <mohnjiles@gmail.com>
2024-08-04 19:37:02 -0700

    big prompt

--- StabilityMatrix.Avalonia/Controls/Inference/PromptCard.axaml.cs
+++ StabilityMatrix.Avalonia/Controls/Inference/PromptCard.axaml.cs
@@ -7,6 +7,7 @@ using AvaloniaEdit.Editing;
 using AvaloniaEdit.Utils;
 using StabilityMatrix.Avalonia.Helpers;
 using StabilityMatrix.Avalonia.Models;
+using StabilityMatrix.Avalonia.ViewModels.Inference;
 using StabilityMatrix.Core.Attributes;
 
 namespace StabilityMatrix.Avalonia.Controls;
@@ -19,6 +20,7 @@ public class PromptCard : TemplatedControlBase
     {
         base.OnApplyTemplate(e);
 
+        FixGrids(e);
         InitializeEditors(e);
     }
 
@@ -58,5 +60,26 @@ public class PromptCard : TemplatedControlBase
                 }
             }
         }
+    }
+
+    private void FixGrids(TemplateAppliedEventArgs e)
+    {
+        if (DataContext is not PromptCardViewModel { IsNegativePromptEnabled: false })
+        {
+            return;
+        }
+
+        // When negative prompt disabled, rearrange grid
+        if (e.NameScope.Find<Grid>("PART_RootGrid") is not { } rootGrid)
+            return;
+
+        // Change `*,16,*,16,Auto` to `*,16,Auto` (Remove index 2 and 3)
+        rootGrid.RowDefinitions.RemoveRange(2, 2);
+
+        // Set the last children to row 2
+        rootGrid.Children[4].SetValue(Grid.RowProperty, 2);
+
+        // Remove the negative prompt row and the separator row (index 2 and 3)
+        rootGrid.Children.RemoveRange(2, 2);
     }
 }

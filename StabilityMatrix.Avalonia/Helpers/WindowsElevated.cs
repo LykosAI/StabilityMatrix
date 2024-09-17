e3de5425dd2b9198e4668632cb6a27814cd3baea
Ionite <dev@ionite.io>
2024-09-16 22:53:29 -0400

    Add SetRegistryValue

--- StabilityMatrix.Avalonia/Helpers/WindowsElevated.cs
+++ StabilityMatrix.Avalonia/Helpers/WindowsElevated.cs
@@ -14,19 +14,54 @@ public static class WindowsElevated
     public static async Task<int> MoveFiles(params (string sourcePath, string targetPath)[] moves)
     {
         // Combine into single command
-        var args = string.Join(" & ", moves.Select(
-            x => $"move \"{x.sourcePath}\" \"{x.targetPath}\""));
-        
+        var args = string.Join(" & ", moves.Select(x => $"move \"{x.sourcePath}\" \"{x.targetPath}\""));
+
         using var process = new Process();
         process.StartInfo.FileName = "cmd.exe";
         process.StartInfo.Arguments = $"/c {args}";
         process.StartInfo.UseShellExecute = true;
         process.StartInfo.CreateNoWindow = true;
         process.StartInfo.Verb = "runas";
-        
+
         process.Start();
         await process.WaitForExitAsync().ConfigureAwait(false);
-        
+
+        return process.ExitCode;
+    }
+
+    /// <summary>
+    /// Set a registry key integer using elevated privileges using powershell.
+    /// </summary>
+    public static async Task<int> SetRegistryValue(string key, string valueName, int value)
+    {
+        using var process = new Process();
+        process.StartInfo.FileName = "reg.exe";
+        process.StartInfo.Arguments = $"add \"{key}\" /v \"{valueName}\" /t REG_DWORD /d {value} /f";
+        process.StartInfo.UseShellExecute = true;
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.Verb = "runas";
+
+        process.Start();
+        await process.WaitForExitAsync().ConfigureAwait(false);
+
+        return process.ExitCode;
+    }
+
+    /// <summary>
+    /// Set a registry key value using elevated privileges using powershell.
+    /// </summary>
+    public static async Task<int> SetRegistryValue(string key, string valueName, string value)
+    {
+        using var process = new Process();
+        process.StartInfo.FileName = "reg.exe";
+        process.StartInfo.Arguments = $"add \"{key}\" /v \"{valueName}\" /t REG_SZ /d \"{value}\" /f";
+        process.StartInfo.UseShellExecute = true;
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.Verb = "runas";
+
+        process.Start();
+        await process.WaitForExitAsync().ConfigureAwait(false);
+
         return process.ExitCode;
     }
 }

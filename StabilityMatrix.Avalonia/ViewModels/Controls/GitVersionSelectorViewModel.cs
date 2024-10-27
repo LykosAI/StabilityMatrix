715f5bc9f7491ecefdf2cdc2698b24e3894b751e
Ionite <dev@ionite.io>
2024-10-27 01:17:09 -0400

    Add GitVersionSelector dialog vm and debug demo

---
+++ StabilityMatrix.Avalonia/ViewModels/Controls/GitVersionSelectorViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia.Data;
+using Avalonia.Layout;
+using Avalonia.Threading;
+using CommunityToolkit.Mvvm.ComponentModel;
+using FluentAvalonia.UI.Controls;
+using StabilityMatrix.Avalonia.Controls;
+using StabilityMatrix.Avalonia.Languages;
+using StabilityMatrix.Core.Git;
+
+namespace StabilityMatrix.Avalonia.ViewModels.Controls;
+
+public partial class GitVersionSelectorViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private IGitVersionProvider? gitVersionProvider;
+
+    public BetterContentDialog GetDialog()
+    {
+        Dispatcher.UIThread.VerifyAccess();
+
+        var selector = new GitVersionSelector
+        {
+            DataContext = this,
+            Height = 400,
+            Width = 600,
+            [!GitVersionSelector.GitVersionProviderProperty] = new Binding(nameof(GitVersionProvider))
+        };
+
+        var dialog = new BetterContentDialog
+        {
+            Content = selector,
+            VerticalAlignment = VerticalAlignment.Stretch,
+            VerticalContentAlignment = VerticalAlignment.Stretch,
+            PrimaryButtonText = Resources.Action_Save,
+            CloseButtonText = Resources.Action_Cancel,
+            DefaultButton = ContentDialogButton.Primary,
+            MinDialogWidth = 400
+        };
+
+        return dialog;
+    }
+}
--- StabilityMatrix.Avalonia/ViewModels/Settings/MainSettingsViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Settings/MainSettingsViewModel.cs
@@ -30,6 +30,7 @@ using NLog;
 using SkiaSharp;
 using StabilityMatrix.Avalonia.Animations;
 using StabilityMatrix.Avalonia.Controls;
+using StabilityMatrix.Avalonia.DesignData;
 using StabilityMatrix.Avalonia.Extensions;
 using StabilityMatrix.Avalonia.Helpers;
 using StabilityMatrix.Avalonia.Languages;
@@ -37,6 +38,7 @@ using StabilityMatrix.Avalonia.Models;
 using StabilityMatrix.Avalonia.Models.TagCompletion;
 using StabilityMatrix.Avalonia.Services;
 using StabilityMatrix.Avalonia.ViewModels.Base;
+using StabilityMatrix.Avalonia.ViewModels.Controls;
 using StabilityMatrix.Avalonia.ViewModels.Dialogs;
 using StabilityMatrix.Avalonia.ViewModels.Inference;
 using StabilityMatrix.Avalonia.Views.Dialogs;
@@ -1036,7 +1038,16 @@ public partial class MainSettingsViewModel : PageViewModelBase
             new CommandItem(DebugExtractImagePromptsToTxtCommand),
             new CommandItem(DebugShowConfirmDeleteDialogCommand),
             new CommandItem(DebugShowModelMetadataEditorDialogCommand),
+            new CommandItem(DebugShowGitVersionSelectorDialogCommand),
         ];
+
+    [RelayCommand]
+    private async Task DebugShowGitVersionSelectorDialog()
+    {
+        var vm = new GitVersionSelectorViewModel { GitVersionProvider = new MockGitVersionProvider() };
+        var dialog = vm.GetDialog();
+        await dialog.ShowAsync();
+    }
 
     [RelayCommand]
     private async Task DebugShowModelMetadataEditorDialog()

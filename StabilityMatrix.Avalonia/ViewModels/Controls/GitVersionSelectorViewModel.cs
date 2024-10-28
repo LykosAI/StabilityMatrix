70cc54caba3c198d49850f240b2b734e1c4b6fdf
Ionite <dev@ionite.io>
2024-10-27 23:43:31 -0400

    Add SelectedGitVersion get set on vm

--- StabilityMatrix.Avalonia/ViewModels/Controls/GitVersionSelectorViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Controls/GitVersionSelectorViewModel.cs
@@ -1,11 +1,14 @@
-﻿using Avalonia.Data;
+﻿using System;
+using Avalonia.Data;
 using Avalonia.Layout;
 using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using FluentAvalonia.UI.Controls;
 using StabilityMatrix.Avalonia.Controls;
+using StabilityMatrix.Avalonia.Controls.Models;
 using StabilityMatrix.Avalonia.Languages;
 using StabilityMatrix.Core.Git;
+using StabilityMatrix.Core.Models;
 
 namespace StabilityMatrix.Avalonia.ViewModels.Controls;
 
@@ -13,6 +16,50 @@ public partial class GitVersionSelectorViewModel : ObservableObject
 {
     [ObservableProperty]
     private IGitVersionProvider? gitVersionProvider;
+
+    [ObservableProperty]
+    private string? selectedBranch;
+
+    [ObservableProperty]
+    private string? selectedCommit;
+
+    [ObservableProperty]
+    private string? selectedTag;
+
+    [ObservableProperty]
+    private GitVersionSelectorVersionType selectedVersionType;
+
+    /// <summary>
+    /// Gets or sets the selected <see cref="GitVersion"/>.
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public GitVersion SelectedGitVersion
+    {
+        get
+        {
+            return SelectedVersionType switch
+            {
+                GitVersionSelectorVersionType.BranchCommit
+                    => new GitVersion { Branch = SelectedBranch, CommitSha = SelectedCommit },
+                GitVersionSelectorVersionType.Tag => new GitVersion { Tag = SelectedTag },
+                _ => throw new InvalidOperationException()
+            };
+        }
+        set
+        {
+            SelectedVersionType = value switch
+            {
+                { Tag: not null } => GitVersionSelectorVersionType.Tag,
+                { Branch: not null, CommitSha: not null } => GitVersionSelectorVersionType.BranchCommit,
+                // Default to branch commit
+                _ => GitVersionSelectorVersionType.BranchCommit
+            };
+
+            SelectedBranch = value.Branch;
+            SelectedCommit = value.CommitSha;
+            SelectedTag = value.Tag;
+        }
+    }
 
     public BetterContentDialog GetDialog()
     {

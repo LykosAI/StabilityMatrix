e6b62ea82bdabc9baa0066b69d838d5930e6a556
Ionite <dev@ionite.io>
2024-11-10 04:38:39 -0500

    Add BetterMarkdownScrollViewer with style

---
+++ StabilityMatrix.Avalonia/Controls/BetterMarkdownScrollViewer.cs
@@ -0,0 +1,13 @@
+﻿using Markdown.Avalonia;
+using StabilityMatrix.Avalonia.Styles.Markdown;
+
+namespace StabilityMatrix.Avalonia.Controls;
+
+public class BetterMarkdownScrollViewer : MarkdownScrollViewer
+{
+    public BetterMarkdownScrollViewer()
+    {
+        MarkdownStyleName = "Empty";
+        MarkdownStyle = new MarkdownStyleFluentAvalonia();
+    }
+}

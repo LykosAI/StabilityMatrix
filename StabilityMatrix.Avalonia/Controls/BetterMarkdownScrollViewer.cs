5f94f80d67998fb4754736975e6b58dc7aef3b52
Ionite <dev@ionite.io>
2024-11-10 04:43:51 -0500

    comment

--- StabilityMatrix.Avalonia/Controls/BetterMarkdownScrollViewer.cs
+++ StabilityMatrix.Avalonia/Controls/BetterMarkdownScrollViewer.cs
@@ -3,6 +3,9 @@ using StabilityMatrix.Avalonia.Styles.Markdown;
 
 namespace StabilityMatrix.Avalonia.Controls;
 
+/// <summary>
+/// Fix MarkdownScrollViewer IBrush errors and not working with Avalonia 11.2.0
+/// </summary>
 public class BetterMarkdownScrollViewer : MarkdownScrollViewer
 {
     public BetterMarkdownScrollViewer()

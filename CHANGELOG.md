cbed93dfc6a933cfd9b4ca1bb0cedd37aed8f7f4
JT <mohnjiles@gmail.com>
2024-12-02 17:16:39 -0800

    Display file size on the CheckpointsPage cards

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to Stability Matrix will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning 2.0](https://semver.org/spec/v2.0.0.html).
 
+## v2.13.0-pre.1
+### Added
+- Added file sizes to the Checkpoint Manager tab
+
 ## v2.13.0-dev.3
 ### Added
 - Added support for SD3.5 in Inference
---
+++ StabilityMatrix.Avalonia/Converters/FileSizeConverters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Avalonia.Data.Converters;
+
+namespace StabilityMatrix.Avalonia.Converters;
+
+[SuppressMessage("ReSharper", "LocalizableElement")]
+public static class FileSizeConverters
+{
+    public static FuncValueConverter<object?, string> HumanizeFileSizeConverter { get; } =
+        new(value =>
+        {
+            var size = Convert.ToDouble(value);
+            string[] sizeUnits = ["B", "KB", "MB", "GB", "TB"];
+            var unitIndex = 0;
+            while (size >= 1000 && unitIndex < sizeUnits.Length - 1)
+            {
+                size /= 1000;
+                unitIndex++;
+            }
+            return $"{size:0.##} {sizeUnits[unitIndex]}";
+        });
+
+    public static FuncValueConverter<object?, string> HumanizeBinaryFileSizeConverter { get; } =
+        new(value =>
+        {
+            var size = Convert.ToDouble(value);
+            string[] sizeUnits = ["B", "KiB", "MiB", "GiB", "TiB"];
+            var unitIndex = 0;
+            while (size >= 1024 && unitIndex < sizeUnits.Length - 1)
+            {
+                size /= 1024;
+                unitIndex++;
+            }
+            return $"{size:0.##} {sizeUnits[unitIndex]}";
+        });
+}

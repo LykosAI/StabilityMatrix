bdf423d5802b21eb473e47bae4140ebf77195c1b
JT <mohnjiles@gmail.com>
2024-12-04 19:34:54 -0800

    Fix build maybe?

--- StabilityMatrix.Avalonia/ViewModels/InstalledWorkflowsViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/InstalledWorkflowsViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Reactive.Linq;
 using AsyncAwaitBestPractices;
 using Avalonia.Controls;
 using Avalonia.Platform.Storage;

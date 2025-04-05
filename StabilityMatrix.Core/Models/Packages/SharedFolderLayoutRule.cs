f8b0b00a98b7bf33e4f0a8e9fe5056cb9df90fa6
Ionite <dev@ionite.io>
2025-04-05 01:46:46 -0400

    Fix SharedFolderLayoutRule Union not copying SourceSubPath

--- StabilityMatrix.Core/Models/Packages/SharedFolderLayoutRule.cs
+++ StabilityMatrix.Core/Models/Packages/SharedFolderLayoutRule.cs
@@ -1,12 +1,12 @@
 ﻿namespace StabilityMatrix.Core.Models.Packages;
 
-public readonly record struct SharedFolderLayoutRule
+public readonly record struct SharedFolderLayoutRule()
 {
-    public SharedFolderType[] SourceTypes { get; init; }
+    public SharedFolderType[] SourceTypes { get; init; } = [];
 
-    public string[] TargetRelativePaths { get; init; }
+    public string[] TargetRelativePaths { get; init; } = [];
 
-    public string[] ConfigDocumentPaths { get; init; }
+    public string[] ConfigDocumentPaths { get; init; } = [];
 
     /// <summary>
     /// For rules that use the root models folder instead of a specific SharedFolderType
@@ -18,30 +18,6 @@ public readonly record struct SharedFolderLayoutRule
     /// </summary>
     public string? SourceSubPath { get; init; }
 
-    public SharedFolderLayoutRule()
-    {
-        SourceTypes = [];
-        TargetRelativePaths = [];
-        ConfigDocumentPaths = [];
-        IsRoot = false;
-    }
-
-    public SharedFolderLayoutRule(SharedFolderType[] types, string[] targets)
-    {
-        SourceTypes = types;
-        TargetRelativePaths = targets;
-        ConfigDocumentPaths = [];
-        IsRoot = false;
-    }
-
-    public SharedFolderLayoutRule(SharedFolderType[] types, string[] targets, string[] configs)
-    {
-        SourceTypes = types;
-        TargetRelativePaths = targets;
-        ConfigDocumentPaths = configs;
-        IsRoot = false;
-    }
-
     public SharedFolderLayoutRule Union(SharedFolderLayoutRule other)
     {
         return this with
@@ -49,7 +25,8 @@ public readonly record struct SharedFolderLayoutRule
             SourceTypes = SourceTypes.Union(other.SourceTypes).ToArray(),
             TargetRelativePaths = TargetRelativePaths.Union(other.TargetRelativePaths).ToArray(),
             ConfigDocumentPaths = ConfigDocumentPaths.Union(other.ConfigDocumentPaths).ToArray(),
-            IsRoot = IsRoot || other.IsRoot
+            IsRoot = IsRoot || other.IsRoot,
+            SourceSubPath = SourceSubPath ?? other.SourceSubPath
         };
     }
 }

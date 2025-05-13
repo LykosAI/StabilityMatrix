8e6e0066fdf33363680a2191241c742e534eb76f
Ionite <dev@ionite.io>
2025-05-12 21:57:34 -0400

    Fix EnVarsViewModel View type & Use new GetDialog

--- StabilityMatrix.Avalonia/ViewModels/Dialogs/EnvVarsViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/Dialogs/EnvVarsViewModel.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
 using Avalonia.Collections;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Injectio.Attributes;
+using StabilityMatrix.Avalonia.Controls;
 using StabilityMatrix.Avalonia.Languages;
 using StabilityMatrix.Avalonia.ViewModels.Base;
+using StabilityMatrix.Avalonia.Views.Dialogs;
 using StabilityMatrix.Core.Attributes;
 using StabilityMatrix.Core.Models;
 
 namespace StabilityMatrix.Avalonia.ViewModels.Dialogs;
 
-[View(typeof(EnvVarsViewModel))]
+[View(typeof(EnvVarsDialog))]
 [ManagedService]
 [RegisterTransient<EnvVarsViewModel>]
 public partial class EnvVarsViewModel : ContentDialogViewModelBase
@@ -42,5 +43,16 @@ public partial class EnvVarsViewModel : ContentDialogViewModelBase
         {
             Debug.WriteLine($"RemoveSelectedRow: Index {selectedIndex} out of range");
         }
+    }
+
+    public override BetterContentDialog GetDialog()
+    {
+        var dialog = base.GetDialog();
+
+        dialog.PrimaryButtonText = Resources.Action_Save;
+        dialog.IsPrimaryButtonEnabled = true;
+        dialog.CloseButtonText = Resources.Action_Cancel;
+
+        return dialog;
     }
 }

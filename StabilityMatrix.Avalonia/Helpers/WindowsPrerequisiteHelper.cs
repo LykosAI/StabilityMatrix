7bb0a76c911c5db735daec9c0dda2eed3ca4a1d0
NeuralFault <65365345+NeuralFault@users.noreply.github.com>
2025-10-27 19:01:28 -0400

    Refactor process command for better readability

--- StabilityMatrix.Avalonia/Helpers/WindowsPrerequisiteHelper.cs
+++ StabilityMatrix.Avalonia/Helpers/WindowsPrerequisiteHelper.cs
@@ -782,7 +782,18 @@ public class WindowsPrerequisiteHelper(
 
         var process = ProcessRunner.StartAnsiProcess(
             VcBuildToolsDownloadPath,
-            "--quiet --wait --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.VC.CMake.Project --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Llvm.Clang --add Microsoft.VisualStudio.Component.Windows10SDK.18362 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 --add Microsoft.VisualStudio.Component.Windows10SDK.20348 --add Microsoft.VisualStudio.Component.Windows11SDK.22000 --add Microsoft.VisualStudio.Component.Windows11SDK.22621 --add Microsoft.VisualStudio.Component.Windows11SDK.26100",
+            "--quiet --wait " +
+            "--add Microsoft.VisualStudio.Workload.VCTools " +
+            "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 " +
+            "--add Microsoft.VisualStudio.Component.VC.CMake.Project " +
+            "--add Microsoft.VisualStudio.Component.VC.Llvm.Clang " +
+            "--add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Llvm.Clang " +
+            "--add Microsoft.VisualStudio.Component.Windows10SDK.18362 " +
+            "--add Microsoft.VisualStudio.Component.Windows10SDK.19041 " +
+            "--add Microsoft.VisualStudio.Component.Windows10SDK.20348 " +
+            "--add Microsoft.VisualStudio.Component.Windows11SDK.22000 " +
+            "--add Microsoft.VisualStudio.Component.Windows11SDK.22621 " +
+            "--add Microsoft.VisualStudio.Component.Windows11SDK.26100",
             outputDataReceived: output =>
                 progress?.Report(
                     new ProgressReport(

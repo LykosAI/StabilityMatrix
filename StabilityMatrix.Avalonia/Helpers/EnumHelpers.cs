e944f5a8c2dcb3b45c91569e885b19b4459db8f8
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2025-12-27 12:03:23 +0100

    Cleaning up EnumHelpers

--- StabilityMatrix.Avalonia/Helpers/EnumHelpers.cs
+++ StabilityMatrix.Avalonia/Helpers/EnumHelpers.cs
@@ -6,23 +6,20 @@ namespace StabilityMatrix.Avalonia.Helpers;
 
 public static class EnumHelpers
 {
-    public static IEnumerable<CivitPeriod> AllCivitPeriods { get; } =
-        Enum.GetValues<CivitPeriod>().Cast<CivitPeriod>();
+    public static IEnumerable<CivitPeriod> AllCivitPeriods { get; } = Enum.GetValues<CivitPeriod>();
 
-    public static IEnumerable<CivitSortMode> AllSortModes { get; } =
-        Enum.GetValues<CivitSortMode>().Cast<CivitSortMode>();
+    public static IEnumerable<CivitSortMode> AllSortModes { get; } = Enum.GetValues<CivitSortMode>();
 
     public static IEnumerable<CivitModelType> AllCivitModelTypes { get; } =
         Enum.GetValues<CivitModelType>()
-            .Cast<CivitModelType>()
             .Where(t => t == CivitModelType.All || t.ConvertTo<SharedFolderType>() > 0)
             .OrderBy(t => t.ToString());
 
     public static IEnumerable<CivitModelType> MetadataEditorCivitModelTypes { get; } =
-        Enum.GetValues<CivitModelType>().Cast<CivitModelType>().OrderBy(t => t.ToString());
+        Enum.GetValues<CivitModelType>().OrderBy(t => t.ToString());
 
     public static IEnumerable<CivitBaseModelType> AllCivitBaseModelTypes { get; } =
-        Enum.GetValues<CivitBaseModelType>().Cast<CivitBaseModelType>();
+        Enum.GetValues<CivitBaseModelType>();
 
     public static IEnumerable<CivitBaseModelType> MetadataEditorCivitBaseModelTypes { get; } =
         AllCivitBaseModelTypes.Where(x => x != CivitBaseModelType.All);

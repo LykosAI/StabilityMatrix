488164de8275a0500d690813db9a54d27e82a7f2
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2025-12-27 11:57:11 +0100

    Minor optimizations and cleaning

--- StabilityMatrix.Avalonia/Helpers/EnumHelpers.cs
+++ StabilityMatrix.Avalonia/Helpers/EnumHelpers.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using StabilityMatrix.Core.Extensions;
+﻿using StabilityMatrix.Core.Extensions;
 using StabilityMatrix.Core.Models;
 using StabilityMatrix.Core.Models.Api;
 
@@ -10,22 +7,22 @@ namespace StabilityMatrix.Avalonia.Helpers;
 public static class EnumHelpers
 {
     public static IEnumerable<CivitPeriod> AllCivitPeriods { get; } =
-        Enum.GetValues(typeof(CivitPeriod)).Cast<CivitPeriod>();
+        Enum.GetValues<CivitPeriod>().Cast<CivitPeriod>();
 
     public static IEnumerable<CivitSortMode> AllSortModes { get; } =
-        Enum.GetValues(typeof(CivitSortMode)).Cast<CivitSortMode>();
+        Enum.GetValues<CivitSortMode>().Cast<CivitSortMode>();
 
     public static IEnumerable<CivitModelType> AllCivitModelTypes { get; } =
-        Enum.GetValues(typeof(CivitModelType))
+        Enum.GetValues<CivitModelType>()
             .Cast<CivitModelType>()
             .Where(t => t == CivitModelType.All || t.ConvertTo<SharedFolderType>() > 0)
             .OrderBy(t => t.ToString());
 
     public static IEnumerable<CivitModelType> MetadataEditorCivitModelTypes { get; } =
-        Enum.GetValues(typeof(CivitModelType)).Cast<CivitModelType>().OrderBy(t => t.ToString());
+        Enum.GetValues<CivitModelType>().Cast<CivitModelType>().OrderBy(t => t.ToString());
 
     public static IEnumerable<CivitBaseModelType> AllCivitBaseModelTypes { get; } =
-        Enum.GetValues(typeof(CivitBaseModelType)).Cast<CivitBaseModelType>();
+        Enum.GetValues<CivitBaseModelType>().Cast<CivitBaseModelType>();
 
     public static IEnumerable<CivitBaseModelType> MetadataEditorCivitBaseModelTypes { get; } =
         AllCivitBaseModelTypes.Where(x => x != CivitBaseModelType.All);
--- StabilityMatrix.Avalonia/ViewModels/ConsoleViewModel.cs
+++ StabilityMatrix.Avalonia/ViewModels/ConsoleViewModel.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using System.Web;
 using Avalonia.Threading;
